dyhkwong/v2ray-core

Refactor ECH (breaking change)

transport/internet/hysteria2/dialer.go
@@ -140,13 +140,12 @@ func (t *transportConnectionState) Close() error {
 
 var MBps uint64 = 1000000 / 8 // MByte
 
-func GetClientTLSConfig(dest net.Destination, streamSettings *internet.MemoryStreamConfig) (*gotls.Config, error) {
+func GetClientTLSConfig(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (*gotls.Config, error) {
 	config := tls.ConfigFromStreamSettings(streamSettings)
 	if config == nil {
 		return nil, newError(Hy2MustNeedTLS)
 	}
-
-	return config.GetTLSConfig(tls.WithDestination(dest), tls.WithNextProto("h3")), nil
+	return config.GetTLSConfig(ctx, tls.WithDestination(dest), tls.WithNextProto("h3"))
 }
 
 func ResolveAddress(ctx context.Context, dest net.Destination, resolver func(ctx context.Context, domain string) net.Address) (net.Addr, error) {
@@ -195,7 +194,7 @@ func (f *connFactory) New(addr net.Addr) (net.PacketConn, error) {
 }
 
 func NewHyClient(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig, resolver func(ctx context.Context, domain string) net.Address) (hyClient.Client, error) {
-	tlsConfig, err := GetClientTLSConfig(dest, streamSettings)
+	tlsConfig, err := GetClientTLSConfig(ctx, dest, streamSettings)
 	if err != nil {
 		return nil, err
 	}

transport/internet/tls/engine.go
@@ -1,11 +1,14 @@
 package tls
 
 import (
+	"context"
+
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/security"
 )
 
 type Engine struct {
+	ctx    context.Context
 	config *Config
 }
 
@@ -21,10 +24,17 @@ func (e *Engine) Client(conn net.Conn, opts ...security.Option) (security.Conn, 
 			return nil, newError("unknown option")
 		}
 	}
-	tlsConn := Client(conn, e.config.GetTLSConfig(options...))
+	tlsConfig, err := e.config.GetTLSConfig(e.ctx, options...)
+	if err != nil {
+		return nil, err
+	}
+	tlsConn := Client(conn, tlsConfig)
 	return tlsConn, nil
 }
 
-func NewTLSSecurityEngineFromConfig(config *Config) (security.Engine, error) {
-	return &Engine{config: config}, nil
+func NewTLSSecurityEngineFromConfig(ctx context.Context, config *Config) (security.Engine, error) {
+	return &Engine{
+		ctx:    ctx,
+		config: config,
+	}, nil
 }

ExclaveNetwork/exclave-core

transport/internet/hysteria2/dialer.go
@@ -140,13 +140,12 @@ func (t *transportConnectionState) Close() error {
 
 var MBps uint64 = 1000000 / 8 // MByte
 
-func GetClientTLSConfig(dest net.Destination, streamSettings *internet.MemoryStreamConfig) (*gotls.Config, error) {
+func GetClientTLSConfig(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (*gotls.Config, error) {
 	config := tls.ConfigFromStreamSettings(streamSettings)
 	if config == nil {
 		return nil, newError(Hy2MustNeedTLS)
 	}
-
-	return config.GetTLSConfig(tls.WithDestination(dest), tls.WithNextProto("h3")), nil
+	return config.GetTLSConfigWithContext(ctx, tls.WithDestination(dest), tls.WithNextProto("h3"))
 }
 
 func ResolveAddress(ctx context.Context, dest net.Destination, resolver func(ctx context.Context, domain string) net.Address) (net.Addr, error) {
@@ -195,7 +194,7 @@ func (f *connFactory) New(addr net.Addr) (net.PacketConn, error) {
 }
 
 func NewHyClient(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig, resolver func(ctx context.Context, domain string) net.Address) (hyClient.Client, error) {
-	tlsConfig, err := GetClientTLSConfig(dest, streamSettings)
+	tlsConfig, err := GetClientTLSConfig(ctx, dest, streamSettings)
 	if err != nil {
 		return nil, err
 	}

transport/internet/tls/engine.go
@@ -1,11 +1,14 @@
 package tls
 
 import (
+	"context"
+
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/security"
 )
 
 type Engine struct {
+	ctx    context.Context
 	config *Config
 }
 
@@ -21,10 +24,17 @@ func (e *Engine) Client(conn net.Conn, opts ...security.Option) (security.Conn, 
 			return nil, newError("unknown option")
 		}
 	}
-	tlsConn := Client(conn, e.config.GetTLSConfig(options...))
+	tlsConfig, err := e.config.GetTLSConfigWithContext(e.ctx, options...)
+	if err != nil {
+		return nil, err
+	}
+	tlsConn := Client(conn, tlsConfig)
 	return tlsConn, nil
 }
 
-func NewTLSSecurityEngineFromConfig(config *Config) (security.Engine, error) {
-	return &Engine{config: config}, nil
+func NewTLSSecurityEngineFromConfig(ctx context.Context, config *Config) (security.Engine, error) {
+	return &Engine{
+		ctx:    ctx,
+		config: config,
+	}, nil
 }