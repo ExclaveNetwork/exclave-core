dyhkwong/v2ray-core

Apply transient storage lifecycle receiver framework to more transports

transport/internet/http/http_test.go
@@ -10,6 +10,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v5/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/protocol/tls/cert"
 	"github.com/v2fly/v2ray-core/v5/testing/servers/tcp"
@@ -51,6 +57,19 @@ func TestHTTPConnection(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("http")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "http",
 		ProtocolSettings: &Config{},

transport/internet/hysteria2/hy2_transport_test.go
@@ -11,6 +11,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v5/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/protocol/tls/cert"
 	"github.com/v2fly/v2ray-core/v5/common/session"
@@ -60,6 +66,19 @@ func TestTCP(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("hysteria2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := hysteria2.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "hysteria2",
 		ProtocolSettings: &hysteria2.Config{Password: "123"},
@@ -128,7 +147,20 @@ func TestUDP(t *testing.T) {
 
 	address, err := net.ParseDestination("udp:127.0.0.1:1180")
 	common.Must(err)
+
 	dctx := session.ContextWithOutbound(context.Background(), &session.Outbound{Target: address})
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("hysteria2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
 
 	conn, err := hysteria2.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "hysteria2",

transport/internet/quic/dialer.go
@@ -10,6 +10,8 @@ import (
 
 	core "github.com/v2fly/v2ray-core/v5"
 	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/task"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
@@ -101,6 +103,18 @@ func (c *clientConnections) cleanConnections() error {
 	return nil
 }
 
+func (c *clientConnections) Close() error {
+	c.access.Lock()
+	for _, cc := range c.conns {
+		_ = cc.conn.CloseWithError(0, "")
+		_ = cc.rawConn.Close()
+	}
+	clear(c.conns)
+	_ = c.cleanup.Close()
+	c.access.Unlock()
+	return nil
+}
+
 func (c *clientConnections) openConnection(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	var destAddr *net.UDPAddr
 	if dest.Address.Family().IsIP() {
@@ -184,18 +198,42 @@ func (c *clientConnections) openConnection(ctx context.Context, dest net.Destina
 	return cc.openStream(destAddr)
 }
 
-var (
-	globalDialerMap    map[dialerConf]*clientConnections
-	globalDialerAccess sync.Mutex
-)
+type transportConnectionState struct {
+	scopedDialerMap    map[dialerConf]*clientConnections
+	scopedDialerAccess sync.Mutex
+}
+
+func (t *transportConnectionState) IsTransientStorageLifecycleReceiver() {
+}
+
+func (t *transportConnectionState) Close() error {
+	t.scopedDialerAccess.Lock()
+	for _, conn := range t.scopedDialerMap {
+		_ = conn.Close()
+	}
+	clear(t.scopedDialerMap)
+	t.scopedDialerAccess.Unlock()
+	return nil
+}
 
 func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	dest.Network = net.Network_UDP
-	globalDialerAccess.Lock()
-	if globalDialerMap == nil {
-		globalDialerMap = make(map[dialerConf]*clientConnections)
+	transportEnvironment := envctx.EnvironmentFromContext(ctx).(environment.TransportEnvironment)
+	state, err := transportEnvironment.TransientStorage().Get(ctx, "quic-transport-connection-state")
+	if err != nil {
+		state = &transportConnectionState{}
+		transportEnvironment.TransientStorage().Put(ctx, "quic-transport-connection-state", state)
+		state, err = transportEnvironment.TransientStorage().Get(ctx, "quic-transport-connection-state")
+		if err != nil {
+			return nil, newError("failed to get quic transport connection state").Base(err)
+		}
 	}
-	client, found := globalDialerMap[dialerConf{dest, streamSettings}]
+	stateTyped := state.(*transportConnectionState)
+	stateTyped.scopedDialerAccess.Lock()
+	if stateTyped.scopedDialerMap == nil {
+		stateTyped.scopedDialerMap = make(map[dialerConf]*clientConnections)
+	}
+	client, found := stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}]
 	if !found {
 		client = new(clientConnections)
 		client.cleanup = &task.Periodic{
@@ -203,9 +241,9 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 			Execute:  client.cleanConnections,
 		}
 		common.Must(client.cleanup.Start())
-		globalDialerMap[dialerConf{dest, streamSettings}] = client
+		stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}] = client
 	}
-	globalDialerAccess.Unlock()
+	stateTyped.scopedDialerAccess.Unlock()
 	return client.openConnection(ctx, dest, streamSettings)
 }
 

transport/internet/quic/quic_test.go
@@ -10,6 +10,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v5/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v5/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/protocol"
 	"github.com/v2fly/v2ray-core/v5/common/protocol/tls/cert"
@@ -60,6 +66,19 @@ func TestQuicConnection(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "quic",
 		ProtocolSettings: &quic.Config{},
@@ -123,6 +142,19 @@ func TestQuicConnectionWithoutTLS(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "quic",
 		ProtocolSettings: &quic.Config{},
@@ -187,6 +219,19 @@ func TestQuicConnectionAuthHeader(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName: "quic",
 		ProtocolSettings: &quic.Config{

ExclaveNetwork/exclave-core

transport/internet/http/http_test.go
@@ -10,6 +10,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v4/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/protocol/tls/cert"
 	"github.com/v2fly/v2ray-core/v4/testing/servers/tcp"
@@ -51,6 +57,19 @@ func TestHTTPConnection(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("http")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "http",
 		ProtocolSettings: &Config{},

transport/internet/hysteria2/hy2_transport_test.go
@@ -11,6 +11,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v4/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/protocol/tls/cert"
 	"github.com/v2fly/v2ray-core/v4/common/session"
@@ -60,6 +66,19 @@ func TestTCP(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("hysteria2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := hysteria2.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "hysteria2",
 		ProtocolSettings: &hysteria2.Config{Password: "123"},
@@ -128,7 +147,20 @@ func TestUDP(t *testing.T) {
 
 	address, err := net.ParseDestination("udp:127.0.0.1:1180")
 	common.Must(err)
+
 	dctx := session.ContextWithOutbound(context.Background(), &session.Outbound{Target: address})
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("hysteria2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
 
 	conn, err := hysteria2.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "hysteria2",

transport/internet/quic/dialer.go
@@ -12,6 +12,8 @@ import (
 
 	core "github.com/v2fly/v2ray-core/v4"
 	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/task"
 	"github.com/v2fly/v2ray-core/v4/features/dns/localdns"
@@ -104,6 +106,18 @@ func (c *clientConnections) cleanConnections() error {
 	return nil
 }
 
+func (c *clientConnections) Close() error {
+	c.access.Lock()
+	for _, cc := range c.conns {
+		_ = cc.conn.CloseWithError(0, "")
+		_ = cc.rawConn.Close()
+	}
+	clear(c.conns)
+	_ = c.cleanup.Close()
+	c.access.Unlock()
+	return nil
+}
+
 func (c *clientConnections) openConnection(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	var destAddr *net.UDPAddr
 	if dest.Address.Family().IsIP() {
@@ -190,18 +204,42 @@ func (c *clientConnections) openConnection(ctx context.Context, dest net.Destina
 	return cc.openStream(destAddr)
 }
 
-var (
-	globalDialerMap    map[dialerConf]*clientConnections
-	globalDialerAccess sync.Mutex
-)
+type transportConnectionState struct {
+	scopedDialerMap    map[dialerConf]*clientConnections
+	scopedDialerAccess sync.Mutex
+}
+
+func (t *transportConnectionState) IsTransientStorageLifecycleReceiver() {
+}
+
+func (t *transportConnectionState) Close() error {
+	t.scopedDialerAccess.Lock()
+	for _, conn := range t.scopedDialerMap {
+		_ = conn.Close()
+	}
+	clear(t.scopedDialerMap)
+	t.scopedDialerAccess.Unlock()
+	return nil
+}
 
 func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	dest.Network = net.Network_UDP
-	globalDialerAccess.Lock()
-	if globalDialerMap == nil {
-		globalDialerMap = make(map[dialerConf]*clientConnections)
+	transportEnvironment := envctx.EnvironmentFromContext(ctx).(environment.TransportEnvironment)
+	state, err := transportEnvironment.TransientStorage().Get(ctx, "quic-transport-connection-state")
+	if err != nil {
+		state = &transportConnectionState{}
+		transportEnvironment.TransientStorage().Put(ctx, "quic-transport-connection-state", state)
+		state, err = transportEnvironment.TransientStorage().Get(ctx, "quic-transport-connection-state")
+		if err != nil {
+			return nil, newError("failed to get quic transport connection state").Base(err)
+		}
 	}
-	client, found := globalDialerMap[dialerConf{dest, streamSettings}]
+	stateTyped := state.(*transportConnectionState)
+	stateTyped.scopedDialerAccess.Lock()
+	if stateTyped.scopedDialerMap == nil {
+		stateTyped.scopedDialerMap = make(map[dialerConf]*clientConnections)
+	}
+	client, found := stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}]
 	if !found {
 		client = new(clientConnections)
 		client.cleanup = &task.Periodic{
@@ -209,9 +247,9 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 			Execute:  client.cleanConnections,
 		}
 		common.Must(client.cleanup.Start())
-		globalDialerMap[dialerConf{dest, streamSettings}] = client
+		stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}] = client
 	}
-	globalDialerAccess.Unlock()
+	stateTyped.scopedDialerAccess.Unlock()
 	return client.openConnection(ctx, dest, streamSettings)
 }
 

transport/internet/quic/quic_test.go
@@ -10,6 +10,12 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/environment/deferredpersistentstorage"
+	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v4/common/environment/filesystemimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/systemnetworkimpl"
+	"github.com/v2fly/v2ray-core/v4/common/environment/transientstorageimpl"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/protocol"
 	"github.com/v2fly/v2ray-core/v4/common/protocol/tls/cert"
@@ -60,6 +66,19 @@ func TestQuicConnection(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "quic",
 		ProtocolSettings: &quic.Config{},
@@ -123,6 +142,19 @@ func TestQuicConnectionWithoutTLS(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName:     "quic",
 		ProtocolSettings: &quic.Config{},
@@ -187,6 +219,19 @@ func TestQuicConnectionAuthHeader(t *testing.T) {
 	time.Sleep(time.Second)
 
 	dctx := context.Background()
+	defaultNetworkImpl := systemnetworkimpl.NewSystemNetworkDefault()
+	defaultFilesystemImpl := filesystemimpl.NewDefaultFileSystemDefaultImpl()
+	deferredPersistentStorageImpl := deferredpersistentstorage.NewDeferredPersistentStorage(dctx)
+	rootEnv := environment.NewRootEnvImpl(dctx,
+		transientstorageimpl.NewScopedTransientStorageImpl(), defaultNetworkImpl.Dialer(), defaultNetworkImpl.Listener(),
+		defaultFilesystemImpl, deferredPersistentStorageImpl)
+	proxyEnvironment := rootEnv.ProxyEnvironment("o")
+	transportEnvironment, err := proxyEnvironment.NarrowScopeToTransport("quic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dctx = envctx.ContextWithEnvironment(dctx, transportEnvironment)
+
 	conn, err := quic.Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
 		ProtocolName: "quic",
 		ProtocolSettings: &quic.Config{