dyhkwong/v2ray-core

Fix uTLS config

transport/internet/tls/utls/utls.go
@@ -106,15 +106,26 @@ func (u uTLSClientConnection) GetConnectionApplicationProtocol() (string, error)
 
 func uTLSConfigFromTLSConfig(config *systls.Config) (*utls.Config, error) { // nolint: unparam
 	uconfig := &utls.Config{
-		Rand:                  config.Rand,
-		Time:                  config.Time,
-		RootCAs:               config.RootCAs,
-		NextProtos:            config.NextProtos,
-		ServerName:            config.ServerName,
-		VerifyPeerCertificate: config.VerifyPeerCertificate,
-		InsecureSkipVerify:    config.InsecureSkipVerify,
-		ClientAuth:            utls.ClientAuthType(config.ClientAuth),
-		ClientCAs:             config.ClientCAs,
+		Rand:                           config.Rand,
+		Time:                           config.Time,
+		RootCAs:                        config.RootCAs,
+		NextProtos:                     config.NextProtos,
+		ServerName:                     config.ServerName,
+		VerifyPeerCertificate:          config.VerifyPeerCertificate,
+		InsecureSkipVerify:             config.InsecureSkipVerify,
+		ClientAuth:                     utls.ClientAuthType(config.ClientAuth),
+		ClientCAs:                      config.ClientCAs,
+		EncryptedClientHelloConfigList: config.EncryptedClientHelloConfigList,
+	}
+	if len(config.Certificates) > 0 {
+		certificates := make([]utls.Certificate, len(config.Certificates))
+		for _, certificate := range config.Certificates {
+			certificates = append(certificates, utls.Certificate{
+				Certificate: certificate.Certificate,
+				PrivateKey:  certificate.PrivateKey,
+			})
+		}
+		uconfig.Certificates = certificates
 	}
 	return uconfig, nil
 }

ExclaveNetwork/exclave-core

transport/internet/tls/utls/utls.go
@@ -108,15 +108,26 @@ func (u uTLSClientConnection) GetConnectionApplicationProtocol() (string, error)
 
 func uTLSConfigFromTLSConfig(config *systls.Config) (*utls.Config, error) { // nolint: unparam
 	uconfig := &utls.Config{
-		Rand:                  config.Rand,
-		Time:                  config.Time,
-		RootCAs:               config.RootCAs,
-		NextProtos:            config.NextProtos,
-		ServerName:            config.ServerName,
-		VerifyPeerCertificate: config.VerifyPeerCertificate,
-		InsecureSkipVerify:    config.InsecureSkipVerify,
-		ClientAuth:            utls.ClientAuthType(config.ClientAuth),
-		ClientCAs:             config.ClientCAs,
+		Rand:                           config.Rand,
+		Time:                           config.Time,
+		RootCAs:                        config.RootCAs,
+		NextProtos:                     config.NextProtos,
+		ServerName:                     config.ServerName,
+		VerifyPeerCertificate:          config.VerifyPeerCertificate,
+		InsecureSkipVerify:             config.InsecureSkipVerify,
+		ClientAuth:                     utls.ClientAuthType(config.ClientAuth),
+		ClientCAs:                      config.ClientCAs,
+		EncryptedClientHelloConfigList: config.EncryptedClientHelloConfigList,
+	}
+	if len(config.Certificates) > 0 {
+		certificates := make([]utls.Certificate, len(config.Certificates))
+		for i, certificate := range config.Certificates {
+			certificates[i] = utls.Certificate{
+				Certificate: certificate.Certificate,
+				PrivateKey:  certificate.PrivateKey,
+			}
+		}
+		uconfig.Certificates = certificates
 	}
 	return uconfig, nil
 }