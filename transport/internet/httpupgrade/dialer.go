parhelia512/v2ray-core

Fix HTTPUpgrade ALPN

transport/internet/httpupgrade/dialer.go
@@ -10,13 +10,17 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/session"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/security"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/transportcommon"
 )
 
 func dialhttpUpgrade(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (net.Conn, error) {
 	transportConfiguration := streamSettings.ProtocolSettings.(*Config)
 
-	conn, err := transportcommon.DialWithSecuritySettings(ctx, dest, streamSettings)
+	conn, err := transportcommon.DialWithSecuritySettings(ctx, dest, streamSettings,
+		security.OptionWithDestination{Dest: dest},
+		security.OptionWithALPN{ALPNs: []string{"http/1.1"}},
+	)
 	if err != nil {
 		return nil, newError("failed to dial request to ", dest).Base(err)
 	}

ExclaveNetwork/exclave-core

transport/internet/httpupgrade/dialer.go
@@ -14,6 +14,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/session"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/security"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/transportcommon"
 )
 
@@ -21,7 +22,10 @@ func dialhttpUpgrade(ctx context.Context, dest net.Destination, streamSettings *
 	transportConfiguration := streamSettings.ProtocolSettings.(*Config)
 
 	dialer := func(earlyData []byte) (net.Conn, io.Reader, error) {
-		conn, err := transportcommon.DialWithSecuritySettings(ctx, dest, streamSettings)
+		conn, err := transportcommon.DialWithSecuritySettings(ctx, dest, streamSettings,
+			security.OptionWithDestination{Dest: dest},
+			security.OptionWithALPN{ALPNs: []string{"http/1.1"}},
+		)
 		if err != nil {
 			return nil, nil, newError("failed to dial request to ", dest).Base(err)
 		}