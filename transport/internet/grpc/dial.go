dyhkwong/v2ray-core

Fix gRPC, HTTP2 and QUIC dialer reuse

transport/internet/grpc/dial.go
@@ -38,8 +38,13 @@ func init() {
 
 type dialerCanceller func()
 
+type dialerConf struct {
+	net.Destination
+	*internet.MemoryStreamConfig
+}
+
 var (
-	globalDialerMap    map[net.Destination]*grpc.ClientConn
+	globalDialerMap    map[dialerConf]*grpc.ClientConn
 	globalDialerAccess sync.Mutex
 )
 
@@ -79,17 +84,17 @@ func getGrpcClient(ctx context.Context, dest net.Destination, dialOption grpc.Di
 	defer globalDialerAccess.Unlock()
 
 	if globalDialerMap == nil {
-		globalDialerMap = make(map[net.Destination]*grpc.ClientConn)
+		globalDialerMap = make(map[dialerConf]*grpc.ClientConn)
 	}
 
 	canceller := func() {
 		globalDialerAccess.Lock()
 		defer globalDialerAccess.Unlock()
-		delete(globalDialerMap, dest)
+		delete(globalDialerMap, dialerConf{dest, streamSettings})
 	}
 
 	// TODO Should support chain proxy to the same destination
-	if client, found := globalDialerMap[dest]; found && client.GetState() != connectivity.Shutdown {
+	if client, found := globalDialerMap[dialerConf{dest, streamSettings}]; found && client.GetState() != connectivity.Shutdown {
 		return client, canceller, nil
 	}
 
@@ -122,6 +127,6 @@ func getGrpcClient(ctx context.Context, dest net.Destination, dialOption grpc.Di
 			return internet.DialSystem(detachedContext, net.TCPDestination(address, port), streamSettings.SocketSettings)
 		}),
 	)
-	globalDialerMap[dest] = conn
+	globalDialerMap[dialerConf{dest, streamSettings}] = conn
 	return conn, canceller, err
 }

ExclaveNetwork/exclave-core

transport/internet/grpc/dial.go
@@ -39,8 +39,13 @@ func init() {
 	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
 }
 
+type dialerConf struct {
+	net.Destination
+	*internet.MemoryStreamConfig
+}
+
 type transportConnectionState struct {
-	scopedDialerMap    map[net.Destination]*grpc.ClientConn
+	scopedDialerMap    map[dialerConf]*grpc.ClientConn
 	scopedDialerAccess sync.Mutex
 }
 
@@ -145,16 +150,16 @@ func getGrpcClient(ctx context.Context, dest net.Destination, streamSettings *in
 	defer stateTyped.scopedDialerAccess.Unlock()
 
 	if stateTyped.scopedDialerMap == nil {
-		stateTyped.scopedDialerMap = make(map[net.Destination]*grpc.ClientConn)
+		stateTyped.scopedDialerMap = make(map[dialerConf]*grpc.ClientConn)
 	}
 
 	canceller := func() {
 		stateTyped.scopedDialerAccess.Lock()
 		defer stateTyped.scopedDialerAccess.Unlock()
-		delete(stateTyped.scopedDialerMap, dest)
+		delete(stateTyped.scopedDialerMap, dialerConf{dest, streamSettings})
 	}
 
-	if client, found := stateTyped.scopedDialerMap[dest]; found && client.GetState() != connectivity.Shutdown {
+	if client, found := stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}]; found && client.GetState() != connectivity.Shutdown {
 		return client, canceller, nil
 	}
 
@@ -165,11 +170,11 @@ func getGrpcClient(ctx context.Context, dest net.Destination, streamSettings *in
 	canceller = func() {
 		stateTyped.scopedDialerAccess.Lock()
 		defer stateTyped.scopedDialerAccess.Unlock()
-		delete(stateTyped.scopedDialerMap, dest)
+		delete(stateTyped.scopedDialerMap, dialerConf{dest, streamSettings})
 		if err != nil {
 			conn.Close()
 		}
 	}
-	stateTyped.scopedDialerMap[dest] = conn
+	stateTyped.scopedDialerMap[dialerConf{dest, streamSettings}] = conn
 	return conn, canceller, err
 }