parhelia512/v2ray-core

Endpoint independent mapping support

app/proxyman/outbound/handler.go
@@ -11,6 +11,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v5/common/mux"
 	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v5/common/net/packetaddr"
 	"github.com/v2fly/v2ray-core/v5/common/serial"
 	"github.com/v2fly/v2ray-core/v5/common/session"
@@ -197,7 +198,7 @@ func (h *Handler) Dial(ctx context.Context, dest net.Destination) (internet.Conn
 				downlinkReader, downlinkWriter := pipe.New(opts...)
 
 				go handler.Dispatch(ctx, &transport.Link{Reader: uplinkReader, Writer: downlinkWriter})
-				conn := net.NewConnection(net.ConnectionInputMulti(uplinkWriter), net.ConnectionOutputMulti(downlinkReader))
+				conn := cnc.NewConnection(cnc.ConnectionInputMulti(uplinkWriter), cnc.ConnectionOutputMulti(downlinkReader))
 
 				securityEngine, err := security.CreateSecurityEngineFromSettings(ctx, h.streamSettings)
 				if err != nil {

common/buf/buffer.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/v2fly/v2ray-core/v5/common/bytespool"
+	"github.com/v2fly/v2ray-core/v5/common/net"
 )
 
 const (
@@ -30,6 +31,7 @@ type Buffer struct {
 	start     int32
 	end       int32
 	ownership ownership
+	Endpoint  *net.Destination
 }
 
 // New creates a Buffer with 0 length and 2K capacity.
@@ -79,6 +81,7 @@ func (b *Buffer) Release() {
 	case bytespools:
 		bytespool.Free(p) // nolint: staticcheck
 	}
+	b.Endpoint = nil
 }
 
 // Clear clears the content of the buffer, results an empty buffer with

functions.go
@@ -7,6 +7,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v5/features/routing"
 	"github.com/v2fly/v2ray-core/v5/transport/internet/udp"
 )
@@ -62,13 +63,13 @@ func Dial(ctx context.Context, v *Instance, dest net.Destination) (net.Conn, err
 	if err != nil {
 		return nil, err
 	}
-	var readerOpt net.ConnectionOption
+	var readerOpt cnc.ConnectionOption
 	if dest.Network == net.Network_TCP {
-		readerOpt = net.ConnectionOutputMulti(r.Reader)
+		readerOpt = cnc.ConnectionOutputMulti(r.Reader)
 	} else {
-		readerOpt = net.ConnectionOutputMultiUDP(r.Reader)
+		readerOpt = cnc.ConnectionOutputMultiUDP(r.Reader)
 	}
-	return net.NewConnection(net.ConnectionInputMulti(r.Writer), readerOpt), nil
+	return cnc.NewConnection(cnc.ConnectionInputMulti(r.Writer), readerOpt), nil
 }
 
 // DialUDP provides a way to exchange UDP packets through V2Ray instance to remote servers.

proxy/loopback/loopback.go
@@ -10,6 +10,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v5/common/retry"
 	"github.com/v2fly/v2ray-core/v5/common/session"
 	"github.com/v2fly/v2ray-core/v5/common/task"
@@ -55,14 +56,14 @@ func (l *Loopback) Process(ctx context.Context, link *transport.Link, _ internet
 			return err
 		}
 
-		var readerOpt net.ConnectionOption
+		var readerOpt cnc.ConnectionOption
 		if dialDest.Network == net.Network_TCP {
-			readerOpt = net.ConnectionOutputMulti(rawConn.Reader)
+			readerOpt = cnc.ConnectionOutputMulti(rawConn.Reader)
 		} else {
-			readerOpt = net.ConnectionOutputMultiUDP(rawConn.Reader)
+			readerOpt = cnc.ConnectionOutputMultiUDP(rawConn.Reader)
 		}
 
-		conn = net.NewConnection(net.ConnectionInputMulti(rawConn.Writer), readerOpt)
+		conn = cnc.NewConnection(cnc.ConnectionInputMulti(rawConn.Writer), readerOpt)
 		return nil
 	})
 	if err != nil {

ExclaveNetwork/exclave-core

app/proxyman/outbound/handler.go
@@ -6,11 +6,11 @@ import (
 	core "github.com/v2fly/v2ray-core/v4"
 	"github.com/v2fly/v2ray-core/v4/app/proxyman"
 	"github.com/v2fly/v2ray-core/v4/common"
-	"github.com/v2fly/v2ray-core/v4/common/dice"
 	"github.com/v2fly/v2ray-core/v4/common/environment"
 	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v4/common/mux"
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v4/common/net/packetaddr"
 	"github.com/v2fly/v2ray-core/v4/common/session"
 	"github.com/v2fly/v2ray-core/v4/features/dns"
@@ -208,7 +208,7 @@ func (h *Handler) Dial(ctx context.Context, dest net.Destination) (internet.Conn
 				downlinkReader, downlinkWriter := pipe.New(opts...)
 
 				go handler.Dispatch(ctx, &transport.Link{Reader: uplinkReader, Writer: downlinkWriter})
-				conn := net.NewConnection(net.ConnectionInputMulti(uplinkWriter), net.ConnectionOutputMulti(downlinkReader))
+				conn := cnc.NewConnection(cnc.ConnectionInputMulti(uplinkWriter), cnc.ConnectionOutputMulti(downlinkReader))
 
 				securityEngine, err := security.CreateSecurityEngineFromSettings(ctx, h.streamSettings)
 				if err != nil {
@@ -289,7 +289,7 @@ func (h *Handler) resolveIP(ctx context.Context, domain string, localAddr net.Ad
 	if len(ips) == 0 {
 		return nil
 	}
-	return net.IPAddress(ips[dice.Roll(len(ips))])
+	return net.IPAddress(ips[0])
 }
 
 func (h *Handler) getStatCouterConnection(conn internet.Connection) internet.Connection {

common/buf/buffer.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/v2fly/v2ray-core/v4/common/bytespool"
+	"github.com/v2fly/v2ray-core/v4/common/net"
 )
 
 const (
@@ -30,6 +31,7 @@ type Buffer struct {
 	start     int32
 	end       int32
 	ownership ownership
+	Endpoint  *net.Destination
 }
 
 // New creates a Buffer with 0 length and 2K capacity.
@@ -79,6 +81,7 @@ func (b *Buffer) Release() {
 	case bytespools:
 		bytespool.Free(p) // nolint: staticcheck
 	}
+	b.Endpoint = nil
 }
 
 // Clear clears the content of the buffer, results an empty buffer with

functions.go
@@ -9,6 +9,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/environment/envctx"
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v4/features/routing"
 	"github.com/v2fly/v2ray-core/v4/transport/internet/udp"
 )
@@ -64,13 +65,13 @@ func Dial(ctx context.Context, v *Instance, dest net.Destination) (net.Conn, err
 	if err != nil {
 		return nil, err
 	}
-	var readerOpt net.ConnectionOption
+	var readerOpt cnc.ConnectionOption
 	if dest.Network == net.Network_TCP {
-		readerOpt = net.ConnectionOutputMulti(r.Reader)
+		readerOpt = cnc.ConnectionOutputMulti(r.Reader)
 	} else {
-		readerOpt = net.ConnectionOutputMultiUDP(r.Reader)
+		readerOpt = cnc.ConnectionOutputMultiUDP(r.Reader)
 	}
-	return net.NewConnection(net.ConnectionInputMulti(r.Writer), readerOpt), nil
+	return cnc.NewConnection(cnc.ConnectionInputMulti(r.Writer), readerOpt), nil
 }
 
 // DialUDP provides a way to exchange UDP packets through V2Ray instance to remote servers.

proxy/loopback/loopback.go
@@ -9,6 +9,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/buf"
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/net/cnc"
 	"github.com/v2fly/v2ray-core/v4/common/retry"
 	"github.com/v2fly/v2ray-core/v4/common/session"
 	"github.com/v2fly/v2ray-core/v4/common/task"
@@ -54,14 +55,14 @@ func (l *Loopback) Process(ctx context.Context, link *transport.Link, _ internet
 			return err
 		}
 
-		var readerOpt net.ConnectionOption
+		var readerOpt cnc.ConnectionOption
 		if dialDest.Network == net.Network_TCP {
-			readerOpt = net.ConnectionOutputMulti(rawConn.Reader)
+			readerOpt = cnc.ConnectionOutputMulti(rawConn.Reader)
 		} else {
-			readerOpt = net.ConnectionOutputMultiUDP(rawConn.Reader)
+			readerOpt = cnc.ConnectionOutputMultiUDP(rawConn.Reader)
 		}
 
-		conn = net.NewConnection(net.ConnectionInputMulti(rawConn.Writer), readerOpt)
+		conn = cnc.NewConnection(cnc.ConnectionInputMulti(rawConn.Writer), readerOpt)
 		return nil
 	})
 	if err != nil {