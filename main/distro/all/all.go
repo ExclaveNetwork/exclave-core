dyhkwong/v2ray-core

Add HTTP3 proxy client

main/distro/all/all.go
@@ -45,6 +45,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/proxy/dokodemo"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/http"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/http3"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/loopback"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks"

proxy/http3/errors.generated.go
@@ -1,0 +1,9 @@
+package http3
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

ExclaveNetwork/exclave-core

main/distro/all/all.go
@@ -43,6 +43,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/proxy/dokodemo"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/http"
+	_ "github.com/v2fly/v2ray-core/v4/proxy/http3"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/loopback"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/mtproto"

proxy/http3/errors.generated.go
@@ -1,0 +1,9 @@
+package http3
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}