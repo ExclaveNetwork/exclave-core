dyhkwong/v2ray-core

Add UID, network type and SSID in routing rule

app/router/command/command.pb.go
@@ -34,6 +34,9 @@ type RoutingContext struct {
 	Attributes        map[string]string      `protobuf:"bytes,10,rep,name=Attributes,proto3" json:"Attributes,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"bytes,2,opt,name=value"`
 	OutboundGroupTags []string               `protobuf:"bytes,11,rep,name=OutboundGroupTags,proto3" json:"OutboundGroupTags,omitempty"`
 	OutboundTag       string                 `protobuf:"bytes,12,opt,name=OutboundTag,proto3" json:"OutboundTag,omitempty"`
+	Uid               uint32                 `protobuf:"varint,13,opt,name=uid,proto3" json:"uid,omitempty"`
+	Ssid              string                 `protobuf:"bytes,14,opt,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType       string                 `protobuf:"bytes,15,opt,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	unknownFields     protoimpl.UnknownFields
 	sizeCache         protoimpl.SizeCache
 }
@@ -148,6 +151,27 @@ func (x *RoutingContext) GetOutboundGroupTags() []string {
 func (x *RoutingContext) GetOutboundTag() string {
 	if x != nil {
 		return x.OutboundTag
+	}
+	return ""
+}
+
+func (x *RoutingContext) GetUid() uint32 {
+	if x != nil {
+		return x.Uid
+	}
+	return 0
+}
+
+func (x *RoutingContext) GetSsid() string {
+	if x != nil {
+		return x.Ssid
+	}
+	return ""
+}
+
+func (x *RoutingContext) GetNetworkType() string {
+	if x != nil {
+		return x.NetworkType
 	}
 	return ""
 }
@@ -637,7 +661,7 @@ var File_app_router_command_command_proto protoreflect.FileDescriptor
 
 const file_app_router_command_command_proto_rawDesc = "" +
 	"\n" +
-	" app/router/command/command.proto\x12\x1dv2ray.core.app.router.command\x1a common/protoext/extensions.proto\x1a\x18common/net/network.proto\"\xa8\x04\n" +
+	" app/router/command/command.proto\x12\x1dv2ray.core.app.router.command\x1a common/protoext/extensions.proto\x1a\x18common/net/network.proto\"\xf1\x04\n" +
 	"\x0eRoutingContext\x12\x1e\n" +
 	"\n" +
 	"InboundTag\x18\x01 \x01(\tR\n" +
@@ -659,7 +683,10 @@ const file_app_router_command_command_proto_rawDesc = "" +
 	" \x03(\v2=.v2ray.core.app.router.command.RoutingContext.AttributesEntryR\n" +
 	"Attributes\x12,\n" +
 	"\x11OutboundGroupTags\x18\v \x03(\tR\x11OutboundGroupTags\x12 \n" +
-	"\vOutboundTag\x18\f \x01(\tR\vOutboundTag\x1a=\n" +
+	"\vOutboundTag\x18\f \x01(\tR\vOutboundTag\x12\x10\n" +
+	"\x03uid\x18\r \x01(\rR\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x0e \x01(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x0f \x01(\tR\vnetworkType\x1a=\n" +
 	"\x0fAttributesEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
 	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"F\n" +

app/router/config.pb.go
@@ -121,6 +121,9 @@ type RoutingRule struct {
 	Protocol       []string      `protobuf:"bytes,9,rep,name=protocol,proto3" json:"protocol,omitempty"`
 	Attributes     string        `protobuf:"bytes,15,opt,name=attributes,proto3" json:"attributes,omitempty"`
 	DomainMatcher  string        `protobuf:"bytes,17,opt,name=domain_matcher,json=domainMatcher,proto3" json:"domain_matcher,omitempty"`
+	Uid            []uint32      `protobuf:"varint,18,rep,packed,name=uid,proto3" json:"uid,omitempty"`
+	Ssid           []string      `protobuf:"bytes,19,rep,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType    []string      `protobuf:"bytes,20,rep,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	// geo_domain instruct simplified config loader to load geo domain rule and fill in domain field.
 	GeoDomain     []*routercommon.GeoSite `protobuf:"bytes,68001,rep,name=geo_domain,json=geoDomain,proto3" json:"geo_domain,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -289,6 +292,27 @@ func (x *RoutingRule) GetDomainMatcher() string {
 		return x.DomainMatcher
 	}
 	return ""
+}
+
+func (x *RoutingRule) GetUid() []uint32 {
+	if x != nil {
+		return x.Uid
+	}
+	return nil
+}
+
+func (x *RoutingRule) GetSsid() []string {
+	if x != nil {
+		return x.Ssid
+	}
+	return nil
+}
+
+func (x *RoutingRule) GetNetworkType() []string {
+	if x != nil {
+		return x.NetworkType
+	}
+	return nil
 }
 
 func (x *RoutingRule) GetGeoDomain() []*routercommon.GeoSite {
@@ -769,6 +793,9 @@ type SimplifiedRoutingRule struct {
 	Protocol       []string `protobuf:"bytes,9,rep,name=protocol,proto3" json:"protocol,omitempty"`
 	Attributes     string   `protobuf:"bytes,15,opt,name=attributes,proto3" json:"attributes,omitempty"`
 	DomainMatcher  string   `protobuf:"bytes,17,opt,name=domain_matcher,json=domainMatcher,proto3" json:"domain_matcher,omitempty"`
+	Uid            []uint32 `protobuf:"varint,18,rep,packed,name=uid,proto3" json:"uid,omitempty"`
+	Ssid           []string `protobuf:"bytes,19,rep,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType    []string `protobuf:"bytes,20,rep,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	// geo_domain instruct simplified config loader to load geo domain rule and fill in domain field.
 	GeoDomain     []*routercommon.GeoSite `protobuf:"bytes,68001,rep,name=geo_domain,json=geoDomain,proto3" json:"geo_domain,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -907,6 +934,27 @@ func (x *SimplifiedRoutingRule) GetDomainMatcher() string {
 	return ""
 }
 
+func (x *SimplifiedRoutingRule) GetUid() []uint32 {
+	if x != nil {
+		return x.Uid
+	}
+	return nil
+}
+
+func (x *SimplifiedRoutingRule) GetSsid() []string {
+	if x != nil {
+		return x.Ssid
+	}
+	return nil
+}
+
+func (x *SimplifiedRoutingRule) GetNetworkType() []string {
+	if x != nil {
+		return x.NetworkType
+	}
+	return nil
+}
+
 func (x *SimplifiedRoutingRule) GetGeoDomain() []*routercommon.GeoSite {
 	if x != nil {
 		return x.GeoDomain
@@ -996,7 +1044,7 @@ var File_app_router_config_proto protoreflect.FileDescriptor
 
 const file_app_router_config_proto_rawDesc = "" +
 	"\n" +
-	"\x17app/router/config.proto\x12\x15v2ray.core.app.router\x1a\x19google/protobuf/any.proto\x1a\x15common/net/port.proto\x1a\x18common/net/network.proto\x1a common/protoext/extensions.proto\x1a$app/router/routercommon/common.proto\"\x80\b\n" +
+	"\x17app/router/config.proto\x12\x15v2ray.core.app.router\x1a\x19google/protobuf/any.proto\x1a\x15common/net/port.proto\x1a\x18common/net/network.proto\x1a common/protoext/extensions.proto\x1a$app/router/routercommon/common.proto\"\xc9\b\n" +
 	"\vRoutingRule\x12\x12\n" +
 	"\x03tag\x18\x01 \x01(\tH\x00R\x03tag\x12%\n" +
 	"\rbalancing_tag\x18\f \x01(\tH\x00R\fbalancingTag\x12B\n" +
@@ -1021,7 +1069,10 @@ const file_app_router_config_proto_rawDesc = "" +
 	"\n" +
 	"attributes\x18\x0f \x01(\tR\n" +
 	"attributes\x12%\n" +
-	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12L\n" +
+	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12\x10\n" +
+	"\x03uid\x18\x12 \x03(\rR\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x13 \x03(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x14 \x03(\tR\vnetworkType\x12L\n" +
 	"\n" +
 	"geo_domain\x18\xa1\x93\x04 \x03(\v2+.v2ray.core.app.router.routercommon.GeoSiteR\tgeoDomainB\f\n" +
 	"\n" +
@@ -1058,7 +1109,7 @@ const file_app_router_config_proto_rawDesc = "" +
 	"\x06Config\x12N\n" +
 	"\x0fdomain_strategy\x18\x01 \x01(\x0e2%.v2ray.core.app.router.DomainStrategyR\x0edomainStrategy\x126\n" +
 	"\x04rule\x18\x02 \x03(\v2\".v2ray.core.app.router.RoutingRuleR\x04rule\x12K\n" +
-	"\x0ebalancing_rule\x18\x03 \x03(\v2$.v2ray.core.app.router.BalancingRuleR\rbalancingRule\"\xab\x05\n" +
+	"\x0ebalancing_rule\x18\x03 \x03(\v2$.v2ray.core.app.router.BalancingRuleR\rbalancingRule\"\xf4\x05\n" +
 	"\x15SimplifiedRoutingRule\x12\x12\n" +
 	"\x03tag\x18\x01 \x01(\tH\x00R\x03tag\x12%\n" +
 	"\rbalancing_tag\x18\f \x01(\tH\x00R\fbalancingTag\x12B\n" +
@@ -1077,7 +1128,10 @@ const file_app_router_config_proto_rawDesc = "" +
 	"\n" +
 	"attributes\x18\x0f \x01(\tR\n" +
 	"attributes\x12%\n" +
-	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12L\n" +
+	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12\x10\n" +
+	"\x03uid\x18\x12 \x03(\rR\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x13 \x03(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x14 \x03(\tR\vnetworkType\x12L\n" +
 	"\n" +
 	"geo_domain\x18\xa1\x93\x04 \x03(\v2+.v2ray.core.app.router.routercommon.GeoSiteR\tgeoDomainB\f\n" +
 	"\n" +

common/session/session.go
@@ -48,6 +48,11 @@ type Inbound struct {
 	Timer *signal.ActivityTimer
 	// Conn is actually internet.Connection. May be nil.
 	Conn net.Conn
+
+	// SagerNet private
+	UID         uint32
+	SSID        string
+	NetworkType string
 }
 
 // Outbound is the metadata of an outbound connection.

features/routing/context.go
@@ -40,4 +40,9 @@ type Context interface {
 
 	// GetSkipDNSResolve returns a flag switch for weather skip dns resolve during route pick.
 	GetSkipDNSResolve() bool
+
+	// SagerNet private
+	GetUid() uint32
+	GetSsid() string
+	GetNetworkType() string
 }

features/routing/session/context.go
@@ -122,6 +122,28 @@ func (ctx *Context) GetSkipDNSResolve() bool {
 	return ctx.Content.SkipDNSResolve
 }
 
+// GetUID implements routing.Context.
+func (ctx *Context) GetUid() uint32 { // nolint: stylecheck
+	if ctx.Inbound == nil {
+		return 0
+	}
+	return ctx.Inbound.UID
+}
+
+func (ctx *Context) GetSsid() string {
+	if ctx.Inbound == nil {
+		return ""
+	}
+	return ctx.Inbound.SSID
+}
+
+func (ctx *Context) GetNetworkType() string {
+	if ctx.Inbound == nil {
+		return ""
+	}
+	return ctx.Inbound.NetworkType
+}
+
 // AsRoutingContext creates a context from context.context with session info.
 func AsRoutingContext(ctx context.Context) routing.Context {
 	return &Context{

infra/conf/rule/rule.go
@@ -204,17 +204,20 @@ func toCidrList(ctx context.Context, ips cfgcommon.StringList) ([]*routercommon.
 func parseFieldRule(ctx context.Context, msg json.RawMessage) (*router.RoutingRule, error) {
 	type RawFieldRule struct {
 		RouterRule
-		Domain     *cfgcommon.StringList  `json:"domain"`
-		Domains    *cfgcommon.StringList  `json:"domains"`
-		IP         *cfgcommon.StringList  `json:"ip"`
-		Port       *cfgcommon.PortList    `json:"port"`
-		Network    *cfgcommon.NetworkList `json:"network"`
-		SourceIP   *cfgcommon.StringList  `json:"source"`
-		SourcePort *cfgcommon.PortList    `json:"sourcePort"`
-		User       *cfgcommon.StringList  `json:"user"`
-		InboundTag *cfgcommon.StringList  `json:"inboundTag"`
-		Protocols  *cfgcommon.StringList  `json:"protocol"`
-		Attributes string                 `json:"attrs"`
+		Domain      *cfgcommon.StringList  `json:"domain"`
+		Domains     *cfgcommon.StringList  `json:"domains"`
+		IP          *cfgcommon.StringList  `json:"ip"`
+		Port        *cfgcommon.PortList    `json:"port"`
+		Network     *cfgcommon.NetworkList `json:"network"`
+		SourceIP    *cfgcommon.StringList  `json:"source"`
+		SourcePort  *cfgcommon.PortList    `json:"sourcePort"`
+		User        *cfgcommon.StringList  `json:"user"`
+		InboundTag  *cfgcommon.StringList  `json:"inboundTag"`
+		Protocols   *cfgcommon.StringList  `json:"protocol"`
+		Attributes  string                 `json:"attrs"`
+		UID         []uint32               `json:"uid"`
+		SSID        []string               `json:"ssid"`
+		NetworkType []string               `json:"networkType"`
 	}
 	rawFieldRule := new(RawFieldRule)
 	err := json.Unmarshal(msg, rawFieldRule)
@@ -308,6 +311,18 @@ func parseFieldRule(ctx context.Context, msg json.RawMessage) (*router.RoutingRu
 
 	if len(rawFieldRule.Attributes) > 0 {
 		rule.Attributes = rawFieldRule.Attributes
+	}
+
+	if len(rawFieldRule.UID) > 0 {
+		rule.Uid = rawFieldRule.UID
+	}
+
+	if len(rawFieldRule.SSID) > 0 {
+		rule.Ssid = rawFieldRule.SSID
+	}
+
+	if len(rawFieldRule.NetworkType) > 0 {
+		rule.NetworkType = rawFieldRule.NetworkType
 	}
 
 	return rule, nil

ExclaveNetwork/exclave-core

app/router/command/command.pb.go
@@ -34,6 +34,9 @@ type RoutingContext struct {
 	Attributes        map[string]string      `protobuf:"bytes,10,rep,name=Attributes,proto3" json:"Attributes,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"bytes,2,opt,name=value"`
 	OutboundGroupTags []string               `protobuf:"bytes,11,rep,name=OutboundGroupTags,proto3" json:"OutboundGroupTags,omitempty"`
 	OutboundTag       string                 `protobuf:"bytes,12,opt,name=OutboundTag,proto3" json:"OutboundTag,omitempty"`
+	Uid               int32                  `protobuf:"varint,13,opt,name=uid,proto3" json:"uid,omitempty"`
+	Ssid              string                 `protobuf:"bytes,14,opt,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType       string                 `protobuf:"bytes,15,opt,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	unknownFields     protoimpl.UnknownFields
 	sizeCache         protoimpl.SizeCache
 }
@@ -148,6 +151,27 @@ func (x *RoutingContext) GetOutboundGroupTags() []string {
 func (x *RoutingContext) GetOutboundTag() string {
 	if x != nil {
 		return x.OutboundTag
+	}
+	return ""
+}
+
+func (x *RoutingContext) GetUid() int32 {
+	if x != nil {
+		return x.Uid
+	}
+	return 0
+}
+
+func (x *RoutingContext) GetSsid() string {
+	if x != nil {
+		return x.Ssid
+	}
+	return ""
+}
+
+func (x *RoutingContext) GetNetworkType() string {
+	if x != nil {
+		return x.NetworkType
 	}
 	return ""
 }
@@ -637,7 +661,7 @@ var File_app_router_command_command_proto protoreflect.FileDescriptor
 
 const file_app_router_command_command_proto_rawDesc = "" +
 	"\n" +
-	" app/router/command/command.proto\x12\x1dv2ray.core.app.router.command\x1a common/protoext/extensions.proto\x1a\x18common/net/network.proto\"\xa8\x04\n" +
+	" app/router/command/command.proto\x12\x1dv2ray.core.app.router.command\x1a common/protoext/extensions.proto\x1a\x18common/net/network.proto\"\xf1\x04\n" +
 	"\x0eRoutingContext\x12\x1e\n" +
 	"\n" +
 	"InboundTag\x18\x01 \x01(\tR\n" +
@@ -659,7 +683,10 @@ const file_app_router_command_command_proto_rawDesc = "" +
 	" \x03(\v2=.v2ray.core.app.router.command.RoutingContext.AttributesEntryR\n" +
 	"Attributes\x12,\n" +
 	"\x11OutboundGroupTags\x18\v \x03(\tR\x11OutboundGroupTags\x12 \n" +
-	"\vOutboundTag\x18\f \x01(\tR\vOutboundTag\x1a=\n" +
+	"\vOutboundTag\x18\f \x01(\tR\vOutboundTag\x12\x10\n" +
+	"\x03uid\x18\r \x01(\x05R\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x0e \x01(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x0f \x01(\tR\vnetworkType\x1a=\n" +
 	"\x0fAttributesEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
 	"\x05value\x18\x02 \x01(\tR\x05value:\x028\x01\"F\n" +

app/router/config.pb.go
@@ -174,6 +174,9 @@ type RoutingRule struct {
 	Attributes     string          `protobuf:"bytes,15,opt,name=attributes,proto3" json:"attributes,omitempty"`
 	DomainMatcher  string          `protobuf:"bytes,17,opt,name=domain_matcher,json=domainMatcher,proto3" json:"domain_matcher,omitempty"`
 	SetAttribute   []*SetAttribute `protobuf:"bytes,18,rep,name=set_attribute,json=setAttribute,proto3" json:"set_attribute,omitempty"`
+	Uid            []int32         `protobuf:"varint,19,rep,packed,name=uid,proto3" json:"uid,omitempty"`
+	Ssid           []string        `protobuf:"bytes,20,rep,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType    []string        `protobuf:"bytes,21,rep,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	// geo_domain instruct simplified config loader to load geo domain rule and fill in domain field.
 	GeoDomain     []*routercommon.GeoSite `protobuf:"bytes,68001,rep,name=geo_domain,json=geoDomain,proto3" json:"geo_domain,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -347,6 +350,27 @@ func (x *RoutingRule) GetDomainMatcher() string {
 func (x *RoutingRule) GetSetAttribute() []*SetAttribute {
 	if x != nil {
 		return x.SetAttribute
+	}
+	return nil
+}
+
+func (x *RoutingRule) GetUid() []int32 {
+	if x != nil {
+		return x.Uid
+	}
+	return nil
+}
+
+func (x *RoutingRule) GetSsid() []string {
+	if x != nil {
+		return x.Ssid
+	}
+	return nil
+}
+
+func (x *RoutingRule) GetNetworkType() []string {
+	if x != nil {
+		return x.NetworkType
 	}
 	return nil
 }
@@ -838,6 +862,9 @@ type SimplifiedRoutingRule struct {
 	Attributes     string          `protobuf:"bytes,15,opt,name=attributes,proto3" json:"attributes,omitempty"`
 	DomainMatcher  string          `protobuf:"bytes,17,opt,name=domain_matcher,json=domainMatcher,proto3" json:"domain_matcher,omitempty"`
 	SetAttribute   []*SetAttribute `protobuf:"bytes,18,rep,name=set_attribute,json=setAttribute,proto3" json:"set_attribute,omitempty"`
+	Uid            []int32         `protobuf:"varint,19,rep,packed,name=uid,proto3" json:"uid,omitempty"`
+	Ssid           []string        `protobuf:"bytes,20,rep,name=ssid,proto3" json:"ssid,omitempty"`
+	NetworkType    []string        `protobuf:"bytes,21,rep,name=network_type,json=networkType,proto3" json:"network_type,omitempty"`
 	// geo_domain instruct simplified config loader to load geo domain rule and fill in domain field.
 	GeoDomain     []*routercommon.GeoSite `protobuf:"bytes,68001,rep,name=geo_domain,json=geoDomain,proto3" json:"geo_domain,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -983,6 +1010,27 @@ func (x *SimplifiedRoutingRule) GetSetAttribute() []*SetAttribute {
 	return nil
 }
 
+func (x *SimplifiedRoutingRule) GetUid() []int32 {
+	if x != nil {
+		return x.Uid
+	}
+	return nil
+}
+
+func (x *SimplifiedRoutingRule) GetSsid() []string {
+	if x != nil {
+		return x.Ssid
+	}
+	return nil
+}
+
+func (x *SimplifiedRoutingRule) GetNetworkType() []string {
+	if x != nil {
+		return x.NetworkType
+	}
+	return nil
+}
+
 func (x *SimplifiedRoutingRule) GetGeoDomain() []*routercommon.GeoSite {
 	if x != nil {
 		return x.GeoDomain
@@ -1075,7 +1123,7 @@ const file_app_router_config_proto_rawDesc = "" +
 	"\x17app/router/config.proto\x12\x15v2ray.core.app.router\x1a\x19google/protobuf/any.proto\x1a\x15common/net/port.proto\x1a\x18common/net/network.proto\x1a common/protoext/extensions.proto\x1a$app/router/routercommon/common.proto\"6\n" +
 	"\fSetAttribute\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
-	"\x05value\x18\x02 \x01(\tR\x05value\"\xca\b\n" +
+	"\x05value\x18\x02 \x01(\tR\x05value\"\x93\t\n" +
 	"\vRoutingRule\x12\x12\n" +
 	"\x03tag\x18\x01 \x01(\tH\x00R\x03tag\x12%\n" +
 	"\rbalancing_tag\x18\f \x01(\tH\x00R\fbalancingTag\x12B\n" +
@@ -1101,7 +1149,10 @@ const file_app_router_config_proto_rawDesc = "" +
 	"attributes\x18\x0f \x01(\tR\n" +
 	"attributes\x12%\n" +
 	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12H\n" +
-	"\rset_attribute\x18\x12 \x03(\v2#.v2ray.core.app.router.SetAttributeR\fsetAttribute\x12L\n" +
+	"\rset_attribute\x18\x12 \x03(\v2#.v2ray.core.app.router.SetAttributeR\fsetAttribute\x12\x10\n" +
+	"\x03uid\x18\x13 \x03(\x05R\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x14 \x03(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x15 \x03(\tR\vnetworkType\x12L\n" +
 	"\n" +
 	"geo_domain\x18\xa1\x93\x04 \x03(\v2+.v2ray.core.app.router.routercommon.GeoSiteR\tgeoDomainB\f\n" +
 	"\n" +
@@ -1139,7 +1190,7 @@ const file_app_router_config_proto_rawDesc = "" +
 	"\x06Config\x12N\n" +
 	"\x0fdomain_strategy\x18\x01 \x01(\x0e2%.v2ray.core.app.router.DomainStrategyR\x0edomainStrategy\x126\n" +
 	"\x04rule\x18\x02 \x03(\v2\".v2ray.core.app.router.RoutingRuleR\x04rule\x12K\n" +
-	"\x0ebalancing_rule\x18\x03 \x03(\v2$.v2ray.core.app.router.BalancingRuleR\rbalancingRule\"\xf5\x05\n" +
+	"\x0ebalancing_rule\x18\x03 \x03(\v2$.v2ray.core.app.router.BalancingRuleR\rbalancingRule\"\xbe\x06\n" +
 	"\x15SimplifiedRoutingRule\x12\x12\n" +
 	"\x03tag\x18\x01 \x01(\tH\x00R\x03tag\x12%\n" +
 	"\rbalancing_tag\x18\f \x01(\tH\x00R\fbalancingTag\x12B\n" +
@@ -1159,7 +1210,10 @@ const file_app_router_config_proto_rawDesc = "" +
 	"attributes\x18\x0f \x01(\tR\n" +
 	"attributes\x12%\n" +
 	"\x0edomain_matcher\x18\x11 \x01(\tR\rdomainMatcher\x12H\n" +
-	"\rset_attribute\x18\x12 \x03(\v2#.v2ray.core.app.router.SetAttributeR\fsetAttribute\x12L\n" +
+	"\rset_attribute\x18\x12 \x03(\v2#.v2ray.core.app.router.SetAttributeR\fsetAttribute\x12\x10\n" +
+	"\x03uid\x18\x13 \x03(\x05R\x03uid\x12\x12\n" +
+	"\x04ssid\x18\x14 \x03(\tR\x04ssid\x12!\n" +
+	"\fnetwork_type\x18\x15 \x03(\tR\vnetworkType\x12L\n" +
 	"\n" +
 	"geo_domain\x18\xa1\x93\x04 \x03(\v2+.v2ray.core.app.router.routercommon.GeoSiteR\tgeoDomainB\f\n" +
 	"\n" +

common/session/session.go
@@ -43,6 +43,11 @@ type Inbound struct {
 	Tag string
 	// User is the user that authencates for the inbound. May be nil if the protocol allows anounymous traffic.
 	User *protocol.MemoryUser
+
+	// SagerNet private
+	UID         int32
+	SSID        string
+	NetworkType string
 }
 
 // Outbound is the metadata of an outbound connection.

features/routing/context.go
@@ -40,4 +40,9 @@ type Context interface {
 
 	// GetSkipDNSResolve returns a flag switch for weather skip dns resolve during route pick.
 	GetSkipDNSResolve() bool
+
+	// SagerNet private
+	GetUid() int32
+	GetSsid() string
+	GetNetworkType() string
 }

features/routing/session/context.go
@@ -122,6 +122,28 @@ func (ctx *Context) GetSkipDNSResolve() bool {
 	return ctx.Content.SkipDNSResolve
 }
 
+// GetUID implements routing.Context.
+func (ctx *Context) GetUid() int32 { // nolint: stylecheck
+	if ctx.Inbound == nil {
+		return 0
+	}
+	return ctx.Inbound.UID
+}
+
+func (ctx *Context) GetSsid() string {
+	if ctx.Inbound == nil {
+		return ""
+	}
+	return ctx.Inbound.SSID
+}
+
+func (ctx *Context) GetNetworkType() string {
+	if ctx.Inbound == nil {
+		return ""
+	}
+	return ctx.Inbound.NetworkType
+}
+
 // AsRoutingContext creates a context from context.context with session info.
 func AsRoutingContext(ctx context.Context) routing.Context {
 	return &Context{

infra/conf/rule/rule.go
@@ -204,17 +204,20 @@ func toCidrList(ctx context.Context, ips cfgcommon.StringList) ([]*routercommon.
 func parseFieldRule(ctx context.Context, msg json.RawMessage) (*router.RoutingRule, error) {
 	type RawFieldRule struct {
 		RouterRule
-		Domain     *cfgcommon.StringList  `json:"domain"`
-		Domains    *cfgcommon.StringList  `json:"domains"`
-		IP         *cfgcommon.StringList  `json:"ip"`
-		Port       *cfgcommon.PortList    `json:"port"`
-		Network    *cfgcommon.NetworkList `json:"network"`
-		SourceIP   *cfgcommon.StringList  `json:"source"`
-		SourcePort *cfgcommon.PortList    `json:"sourcePort"`
-		User       *cfgcommon.StringList  `json:"user"`
-		InboundTag *cfgcommon.StringList  `json:"inboundTag"`
-		Protocols  *cfgcommon.StringList  `json:"protocol"`
-		Attributes string                 `json:"attrs"`
+		Domain      *cfgcommon.StringList  `json:"domain"`
+		Domains     *cfgcommon.StringList  `json:"domains"`
+		IP          *cfgcommon.StringList  `json:"ip"`
+		Port        *cfgcommon.PortList    `json:"port"`
+		Network     *cfgcommon.NetworkList `json:"network"`
+		SourceIP    *cfgcommon.StringList  `json:"source"`
+		SourcePort  *cfgcommon.PortList    `json:"sourcePort"`
+		User        *cfgcommon.StringList  `json:"user"`
+		InboundTag  *cfgcommon.StringList  `json:"inboundTag"`
+		Protocols   *cfgcommon.StringList  `json:"protocol"`
+		Attributes  string                 `json:"attrs"`
+		UID         []int32                `json:"uid"`
+		SSID        []string               `json:"ssid"`
+		NetworkType []string               `json:"networkType"`
 	}
 	rawFieldRule := new(RawFieldRule)
 	err := json.Unmarshal(msg, rawFieldRule)
@@ -308,6 +311,18 @@ func parseFieldRule(ctx context.Context, msg json.RawMessage) (*router.RoutingRu
 
 	if len(rawFieldRule.Attributes) > 0 {
 		rule.Attributes = rawFieldRule.Attributes
+	}
+
+	if len(rawFieldRule.UID) > 0 {
+		rule.Uid = rawFieldRule.UID
+	}
+
+	if len(rawFieldRule.SSID) > 0 {
+		rule.Ssid = rawFieldRule.SSID
+	}
+
+	if len(rawFieldRule.NetworkType) > 0 {
+		rule.NetworkType = rawFieldRule.NetworkType
 	}
 
 	return rule, nil