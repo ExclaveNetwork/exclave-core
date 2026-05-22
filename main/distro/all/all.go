dyhkwong/v2ray-core

Add HTTP+SOCKS mixed inbound

main/distro/all/all.go
@@ -45,6 +45,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/http"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/loopback"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks_2022"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/socks"
@@ -121,6 +122,7 @@ import (
 
 	// Simplified config
 	_ "github.com/v2fly/v2ray-core/v5/proxy/http/simplified"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/mixed/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/socks/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/trojan/simplified"

proxy/mixed/bufconn.go
@@ -1,0 +1,57 @@
+package mixed
+
+import (
+	"bufio"
+	"time"
+
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+)
+
+type BufferedConnection struct {
+	reader *bufio.Reader
+	conn   internet.Connection
+}
+
+func NewBufferedConnection(conn internet.Connection) BufferedConnection {
+	return BufferedConnection{
+		reader: bufio.NewReader(conn),
+		conn:   conn,
+	}
+}
+
+func (c BufferedConnection) Peek(n int) ([]byte, error) {
+	return c.reader.Peek(n)
+}
+
+func (c BufferedConnection) Read(p []byte) (int, error) {
+	return c.reader.Read(p)
+}
+
+func (c BufferedConnection) Write(b []byte) (n int, err error) {
+	return c.conn.Write(b)
+}
+
+func (c BufferedConnection) Close() error {
+	return c.conn.Close()
+}
+
+func (c BufferedConnection) LocalAddr() net.Addr {
+	return c.conn.LocalAddr()
+}
+
+func (c BufferedConnection) RemoteAddr() net.Addr {
+	return c.conn.RemoteAddr()
+}
+
+func (c BufferedConnection) SetDeadline(t time.Time) error {
+	return c.conn.SetDeadline(t)
+}
+
+func (c BufferedConnection) SetReadDeadline(t time.Time) error {
+	return c.conn.SetReadDeadline(t)
+}
+
+func (c BufferedConnection) SetWriteDeadline(t time.Time) error {
+	return c.conn.SetWriteDeadline(t)
+}

proxy/mixed/errors.generated.go
@@ -1,0 +1,9 @@
+package mixed
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

proxy/mixed/server.go
@@ -1,0 +1,152 @@
+package mixed
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/features/routing"
+	"github.com/v2fly/v2ray-core/v5/proxy/http"
+	"github.com/v2fly/v2ray-core/v5/proxy/socks"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+)
+
+const (
+	socks5Version = 0x05
+	socks4Version = 0x04
+)
+
+// Server is a Mixed proxy server
+type Server struct {
+	httpServer        http.Server
+	socksServer       socks.Server
+	socksOnlyNetworks []net.Network
+	httpOnlyNetworks  []net.Network
+	intersectNetworks []net.Network
+}
+
+// NewServer creates a new Server object.
+func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
+	httpServer, err := http.NewServer(
+		ctx,
+		&http.ServerConfig{
+			Timeout:          config.Timeout,
+			Accounts:         config.Accounts,
+			AllowTransparent: config.AllowTransparent,
+			UserLevel:        config.UserLevel,
+		},
+	)
+	if err != nil {
+		return nil, newError("Errors in http config").Base(err).AtError()
+	}
+	socksAuthType := socks.AuthType_NO_AUTH
+	if len(config.Accounts) > 0 {
+		socksAuthType = socks.AuthType_PASSWORD
+	}
+	socksServer, err := socks.NewServer(
+		ctx,
+		&socks.ServerConfig{
+			AuthType:       socksAuthType,
+			Accounts:       config.Accounts,
+			Address:        config.Address,
+			UdpEnabled:     config.UdpEnabled,
+			Timeout:        config.Timeout,
+			UserLevel:      config.UserLevel,
+			PacketEncoding: config.PacketEncoding,
+		})
+	if err != nil {
+		return nil, newError("Errors in socks config").Base(err).AtError()
+	}
+
+	socksNetworks := socksServer.Network()
+	httpNetworks := httpServer.Network()
+
+	socksOnlyNetworks := make([]net.Network, 0)
+	httpOnlyNetworks := make([]net.Network, 0)
+	intersectNetworks := make([]net.Network, 0)
+
+	for _, n := range socksNetworks {
+		if !isInNetworkSlice(n, &httpNetworks) {
+			socksOnlyNetworks = append(socksOnlyNetworks, n)
+		} else {
+			intersectNetworks = append(intersectNetworks, n)
+		}
+	}
+
+	for _, n := range httpNetworks {
+		if !isInNetworkSlice(n, &socksNetworks) {
+			httpOnlyNetworks = append(httpOnlyNetworks, n)
+		}
+	}
+
+	s := &Server{
+		httpServer:        *httpServer,
+		socksServer:       *socksServer,
+		socksOnlyNetworks: socksOnlyNetworks,
+		httpOnlyNetworks:  httpOnlyNetworks,
+		intersectNetworks: intersectNetworks,
+	}
+	return s, nil
+}
+
+// Network implements proxy.Inbound.
+func (s *Server) Network() []net.Network {
+	returnNetwork := s.intersectNetworks
+	returnNetwork = append(returnNetwork, s.socksOnlyNetworks...)
+	returnNetwork = append(returnNetwork, s.httpOnlyNetworks...)
+	return returnNetwork
+}
+
+// Process implements proxy.Inbound.
+func (s *Server) Process(ctx context.Context, network net.Network, conn internet.Connection, dispatcher routing.Dispatcher) error {
+	// Socks only
+	if isInNetworkSlice(network, &s.socksOnlyNetworks) {
+		newError("Connection is identified as Socks").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.socksServer.Process(ctx, network, conn, dispatcher)
+	}
+
+	// HTTP only
+	if isInNetworkSlice(network, &s.httpOnlyNetworks) {
+		newError("Connection is identified as HTTP").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.httpServer.Process(ctx, network, conn, dispatcher)
+	}
+
+	// no UDP for BufferedConnection
+	if network == net.Network_UDP {
+		return newError("UDP is only available for Socks").AtError()
+	}
+
+	// read first byte to distinguish HTTP and Socks
+	bufferedConnection := NewBufferedConnection(conn)
+	firstByte, err := bufferedConnection.Peek(1)
+	if err != nil {
+		return newError("Read first byte failed").Base(err).AtError()
+	}
+	newError("First byte", firstByte).AtDebug().WriteToLog(session.ExportIDToError(ctx))
+
+	if firstByte[0] == socks4Version || firstByte[0] == socks5Version {
+		newError("Connection is identified as Socks").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.socksServer.Process(ctx, network, bufferedConnection, dispatcher)
+	}
+	newError("Connection is identified as HTTP").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+	return s.httpServer.Process(ctx, network, bufferedConnection, dispatcher)
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ServerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewServer(ctx, config.(*ServerConfig))
+	}))
+}
+
+// isInNetworkSlice gets whether the network is in slice
+func isInNetworkSlice(network net.Network, networks *[]net.Network) bool {
+	found := false
+	for _, n := range *networks {
+		if network == n {
+			found = true
+			break
+		}
+	}
+	return found
+}

ExclaveNetwork/exclave-core

main/distro/all/all.go
@@ -44,6 +44,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/http"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/loopback"
+	_ "github.com/v2fly/v2ray-core/v4/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/mtproto"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/shadowsocks"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/shadowsocks_2022"

proxy/mixed/bufconn.go
@@ -1,0 +1,59 @@
+//go:build !confonly
+
+package mixed
+
+import (
+	"bufio"
+	"time"
+
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+)
+
+type BufferedConnection struct {
+	reader *bufio.Reader
+	conn   internet.Connection
+}
+
+func NewBufferedConnection(conn internet.Connection) BufferedConnection {
+	return BufferedConnection{
+		reader: bufio.NewReader(conn),
+		conn:   conn,
+	}
+}
+
+func (c BufferedConnection) Peek(n int) ([]byte, error) {
+	return c.reader.Peek(n)
+}
+
+func (c BufferedConnection) Read(p []byte) (int, error) {
+	return c.reader.Read(p)
+}
+
+func (c BufferedConnection) Write(b []byte) (n int, err error) {
+	return c.conn.Write(b)
+}
+
+func (c BufferedConnection) Close() error {
+	return c.conn.Close()
+}
+
+func (c BufferedConnection) LocalAddr() net.Addr {
+	return c.conn.LocalAddr()
+}
+
+func (c BufferedConnection) RemoteAddr() net.Addr {
+	return c.conn.RemoteAddr()
+}
+
+func (c BufferedConnection) SetDeadline(t time.Time) error {
+	return c.conn.SetDeadline(t)
+}
+
+func (c BufferedConnection) SetReadDeadline(t time.Time) error {
+	return c.conn.SetReadDeadline(t)
+}
+
+func (c BufferedConnection) SetWriteDeadline(t time.Time) error {
+	return c.conn.SetWriteDeadline(t)
+}

proxy/mixed/errors.generated.go
@@ -1,0 +1,9 @@
+package mixed
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

proxy/mixed/server.go
@@ -1,0 +1,155 @@
+//go:build !confonly
+
+package mixed
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/session"
+	"github.com/v2fly/v2ray-core/v4/features/routing"
+	"github.com/v2fly/v2ray-core/v4/proxy/http"
+	"github.com/v2fly/v2ray-core/v4/proxy/socks"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+)
+
+const (
+	socks5Version = 0x05
+	socks4Version = 0x04
+)
+
+// Server is a Mixed proxy server
+type Server struct {
+	httpServer        http.Server
+	socksServer       socks.Server
+	socksOnlyNetworks []net.Network
+	httpOnlyNetworks  []net.Network
+	intersectNetworks []net.Network
+}
+
+// NewServer creates a new Server object.
+func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
+	httpServer, err := http.NewServer(
+		ctx,
+		&http.ServerConfig{
+			Timeout:          config.Timeout,
+			Accounts:         config.Accounts,
+			AllowTransparent: config.AllowTransparent,
+			UserLevel:        config.UserLevel,
+		},
+	)
+	if err != nil {
+		return nil, newError("Errors in http config").Base(err).AtError()
+	}
+	socksAuthType := socks.AuthType_NO_AUTH
+	if len(config.Accounts) > 0 {
+		socksAuthType = socks.AuthType_PASSWORD
+	}
+	socksServer, err := socks.NewServer(
+		ctx,
+		&socks.ServerConfig{
+			AuthType:       socksAuthType,
+			Accounts:       config.Accounts,
+			Address:        config.Address,
+			UdpEnabled:     config.UdpEnabled,
+			Timeout:        config.Timeout,
+			UserLevel:      config.UserLevel,
+			PacketEncoding: config.PacketEncoding,
+			DeferLastReply: config.DeferLastReply,
+		})
+	if err != nil {
+		return nil, newError("Errors in socks config").Base(err).AtError()
+	}
+
+	socksNetworks := socksServer.Network()
+	httpNetworks := httpServer.Network()
+
+	socksOnlyNetworks := make([]net.Network, 0)
+	httpOnlyNetworks := make([]net.Network, 0)
+	intersectNetworks := make([]net.Network, 0)
+
+	for _, n := range socksNetworks {
+		if !isInNetworkSlice(n, &httpNetworks) {
+			socksOnlyNetworks = append(socksOnlyNetworks, n)
+		} else {
+			intersectNetworks = append(intersectNetworks, n)
+		}
+	}
+
+	for _, n := range httpNetworks {
+		if !isInNetworkSlice(n, &socksNetworks) {
+			httpOnlyNetworks = append(httpOnlyNetworks, n)
+		}
+	}
+
+	s := &Server{
+		httpServer:        *httpServer,
+		socksServer:       *socksServer,
+		socksOnlyNetworks: socksOnlyNetworks,
+		httpOnlyNetworks:  httpOnlyNetworks,
+		intersectNetworks: intersectNetworks,
+	}
+	return s, nil
+}
+
+// Network implements proxy.Inbound.
+func (s *Server) Network() []net.Network {
+	returnNetwork := s.intersectNetworks
+	returnNetwork = append(returnNetwork, s.socksOnlyNetworks...)
+	returnNetwork = append(returnNetwork, s.httpOnlyNetworks...)
+	return returnNetwork
+}
+
+// Process implements proxy.Inbound.
+func (s *Server) Process(ctx context.Context, network net.Network, conn internet.Connection, dispatcher routing.Dispatcher) error {
+	// Socks only
+	if isInNetworkSlice(network, &s.socksOnlyNetworks) {
+		newError("Connection is identified as Socks").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.socksServer.Process(ctx, network, conn, dispatcher)
+	}
+
+	// HTTP only
+	if isInNetworkSlice(network, &s.httpOnlyNetworks) {
+		newError("Connection is identified as HTTP").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.httpServer.Process(ctx, network, conn, dispatcher)
+	}
+
+	// no UDP for BufferedConnection
+	if network == net.Network_UDP {
+		return newError("UDP is only available for Socks").AtError()
+	}
+
+	// read first byte to distinguish HTTP and Socks
+	bufferedConnection := NewBufferedConnection(conn)
+	firstByte, err := bufferedConnection.Peek(1)
+	if err != nil {
+		return newError("Read first byte failed").Base(err).AtError()
+	}
+	newError("First byte", firstByte).AtDebug().WriteToLog(session.ExportIDToError(ctx))
+
+	if firstByte[0] == socks4Version || firstByte[0] == socks5Version {
+		newError("Connection is identified as Socks").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+		return s.socksServer.Process(ctx, network, bufferedConnection, dispatcher)
+	}
+	newError("Connection is identified as HTTP").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+	return s.httpServer.Process(ctx, network, bufferedConnection, dispatcher)
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ServerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewServer(ctx, config.(*ServerConfig))
+	}))
+}
+
+// isInNetworkSlice gets whether the network is in slice
+func isInNetworkSlice(network net.Network, networks *[]net.Network) bool {
+	found := false
+	for _, n := range *networks {
+		if network == n {
+			found = true
+			break
+		}
+	}
+	return found
+}