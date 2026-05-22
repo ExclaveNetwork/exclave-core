dyhkwong/v2ray-core

Support non-A/AAAA DNS queries

app/dns/nameserver_local.go
@@ -2,9 +2,11 @@ package dns
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/session"
 	"github.com/v2fly/v2ray-core/v5/features/dns"
 	"github.com/v2fly/v2ray-core/v5/features/dns/localdns"
 )
@@ -15,7 +17,8 @@ type LocalNameServer struct {
 }
 
 // QueryIPWithTTL implements ServerWithTTL.
-func (s *LocalNameServer) QueryIPWithTTL(_ context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, uint32, time.Time, error) {
+func (s *LocalNameServer) QueryIPWithTTL(ctx context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, uint32, time.Time, error) {
+	newError("localhost querying: ", domain).AtInfo().WriteToLog(session.ExportIDToError(ctx))
 	var ips []net.IP
 	ttl := uint32(600)
 	expireAt := time.Now().Add(time.Duration(ttl) * time.Second)
@@ -31,16 +34,26 @@ func (s *LocalNameServer) QueryIPWithTTL(_ context.Context, domain string, _ net
 	}
 
 	if len(ips) > 0 {
-		newError("Localhost got answer: ", domain, " -> ", ips).AtInfo().WriteToLog()
+		newError("localhost got answer: ", domain, " -> ", ips).AtInfo().WriteToLog()
 	}
 
 	return ips, ttl, expireAt, err
 }
 
 // QueryIP implements Server.
-func (s *LocalNameServer) QueryIP(_ context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, error) {
-	ips, _, _, err := s.QueryIPWithTTL(context.TODO(), domain, nil, option, false)
+func (s *LocalNameServer) QueryIP(ctx context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, error) {
+	ips, _, _, err := s.QueryIPWithTTL(ctx, domain, nil, option, false)
 	return ips, err
+}
+
+// NewReqID implements ServerRaw.
+func (s *LocalNameServer) NewReqID() uint16 {
+	return uint16(rand.Intn(65536))
+}
+
+// QueryRaw implements ServerRaw.
+func (s *LocalNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	return s.client.QueryRaw(request)
 }
 
 // Name implements Server.

app/dns/nameserver_quic.go
@@ -179,14 +179,14 @@ func (s *QUICNameServer) updateIP(req *dnsRequest, ipRec *IPRecord) {
 	common.Must(s.cleanup.Start())
 }
 
-func (s *QUICNameServer) newReqID() uint16 {
+func (s *QUICNameServer) NewReqID() uint16 {
 	return 0
 }
 
 func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP net.IP, option dns_feature.IPOption) {
 	newError(s.name, " querying: ", domain).AtInfo().WriteToLog(session.ExportIDToError(ctx))
 
-	reqs := buildReqMsgs(domain, option, s.newReqID, genEDNS0Options(clientIP))
+	reqs := buildReqMsgs(domain, option, s.NewReqID, genEDNS0Options(clientIP))
 
 	var deadline time.Time
 	if d, ok := ctx.Deadline(); ok {
@@ -267,6 +267,71 @@ func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP 
 			}
 			s.updateIP(r, rec)
 		}(req)
+	}
+}
+
+func (s *QUICNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	var deadline time.Time
+	if d, ok := ctx.Deadline(); ok {
+		deadline = d
+	} else {
+		deadline = time.Now().Add(time.Second * 5)
+	}
+	dnsCtx := ctx
+	if inbound := session.InboundFromContext(ctx); inbound != nil {
+		dnsCtx = session.ContextWithInbound(dnsCtx, inbound)
+	}
+	dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
+		Protocol:       "quic",
+		SkipDNSResolve: true,
+	})
+	var cancel context.CancelFunc
+	dnsCtx, cancel = context.WithDeadline(dnsCtx, deadline)
+	defer cancel()
+	requestBuf := buf.New()
+	defer requestBuf.Release()
+	binary.Write(requestBuf, binary.BigEndian, uint16(len(request)))
+	requestBuf.Write(request)
+	responseBuf := buf.New()
+	defer responseBuf.Release()
+	done := make(chan interface{})
+	var retErr error
+	go func() {
+		defer close(done)
+		conn, err := s.openStream(dnsCtx)
+		if err != nil {
+			retErr = newError("failed to open quic connection")
+			return
+		}
+		_, err = conn.Write(requestBuf.Bytes())
+		if err != nil {
+			retErr = newError("failed to send query")
+			return
+		}
+		_ = conn.Close()
+		n, err := responseBuf.ReadFullFrom(conn, 2)
+		if err != nil && n == 0 {
+			retErr = newError("failed to read response length").Base(err)
+			return
+		}
+		var length int16
+		err = binary.Read(bytes.NewReader(responseBuf.Bytes()), binary.BigEndian, &length)
+		if err != nil {
+			retErr = newError("failed to parse response length").Base(err)
+			return
+		}
+		responseBuf.Clear()
+		n, err = responseBuf.ReadFullFrom(conn, int32(length))
+		if err != nil && n == 0 {
+			retErr = newError("failed to read response length").Base(err)
+			return
+		}
+	}()
+	select {
+	case <-dnsCtx.Done():
+		return nil, dnsCtx.Err()
+	case <-done:
+		return responseBuf.Bytes(), retErr
 	}
 }
 

app/dns/nameserver_tcp.go
@@ -177,14 +177,14 @@ func (s *TCPNameServer) updateIP(req *dnsRequest, ipRec *IPRecord) {
 	common.Must(s.cleanup.Start())
 }
 
-func (s *TCPNameServer) newReqID() uint16 {
+func (s *TCPNameServer) NewReqID() uint16 {
 	return uint16(atomic.AddUint32(&s.reqID, 1))
 }
 
 func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP net.IP, option dns_feature.IPOption) {
 	newError(s.name, " querying DNS for: ", domain).AtDebug().WriteToLog(session.ExportIDToError(ctx))
 
-	reqs := buildReqMsgs(domain, option, s.newReqID, genEDNS0Options(clientIP))
+	reqs := buildReqMsgs(domain, option, s.NewReqID, genEDNS0Options(clientIP))
 
 	var deadline time.Time
 	if d, ok := ctx.Deadline(); ok {
@@ -262,6 +262,71 @@ func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP n
 
 			s.updateIP(r, rec)
 		}(req)
+	}
+}
+
+func (s *TCPNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	var deadline time.Time
+	if d, ok := ctx.Deadline(); ok {
+		deadline = d
+	} else {
+		deadline = time.Now().Add(time.Second * 5)
+	}
+	dnsCtx := ctx
+	if inbound := session.InboundFromContext(ctx); inbound != nil {
+		dnsCtx = session.ContextWithInbound(dnsCtx, inbound)
+	}
+	dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
+		Protocol:       s.protocol,
+		SkipDNSResolve: true,
+	})
+	var cancel context.CancelFunc
+	dnsCtx, cancel = context.WithDeadline(dnsCtx, deadline)
+	defer cancel()
+	requestBuf := buf.New()
+	defer requestBuf.Release()
+	binary.Write(requestBuf, binary.BigEndian, uint16(len(request)))
+	requestBuf.Write(request)
+	responseBuf := buf.New()
+	defer responseBuf.Release()
+	done := make(chan interface{})
+	var retErr error
+	go func() {
+		defer close(done)
+		conn, err := s.dial(dnsCtx)
+		if err != nil {
+			retErr = newError("failed to dial namesever").Base(err)
+			return
+		}
+		defer conn.Close()
+		_, err = conn.Write(requestBuf.Bytes())
+		if err != nil {
+			retErr = newError("failed to send query").Base(err)
+			return
+		}
+		n, err := responseBuf.ReadFullFrom(conn, 2)
+		if err != nil && n == 0 {
+			retErr = newError("failed to read response length").Base(err)
+			return
+		}
+		var length int16
+		err = binary.Read(bytes.NewReader(responseBuf.Bytes()), binary.BigEndian, &length)
+		if err != nil {
+			retErr = newError("failed to parse response length").Base(err)
+			return
+		}
+		responseBuf.Clear()
+		n, err = responseBuf.ReadFullFrom(conn, int32(length))
+		if err != nil && n == 0 {
+			retErr = newError("failed to read response length").Base(err)
+			return
+		}
+	}()
+	select {
+	case <-dnsCtx.Done():
+		return nil, dnsCtx.Err()
+	case <-done:
+		return responseBuf.Bytes(), retErr
 	}
 }
 

ExclaveNetwork/exclave-core

app/dns/nameserver_local.go
@@ -4,9 +4,11 @@ package dns
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/session"
 	"github.com/v2fly/v2ray-core/v4/features/dns"
 	"github.com/v2fly/v2ray-core/v4/features/dns/localdns"
 )
@@ -17,7 +19,8 @@ type LocalNameServer struct {
 }
 
 // QueryIPWithTTL implements ServerWithTTL.
-func (s *LocalNameServer) QueryIPWithTTL(_ context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, time.Time, error) {
+func (s *LocalNameServer) QueryIPWithTTL(ctx context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, time.Time, error) {
+	newError("localhost querying: ", domain).AtInfo().WriteToLog(session.ExportIDToError(ctx))
 	var ips []net.IP
 	var err error
 
@@ -31,16 +34,26 @@ func (s *LocalNameServer) QueryIPWithTTL(_ context.Context, domain string, _ net
 	}
 
 	if len(ips) > 0 {
-		newError("Localhost got answer: ", domain, " -> ", ips).AtInfo().WriteToLog()
+		newError("localhost got answer: ", domain, " -> ", ips).AtInfo().WriteToLog()
 	}
 
 	return ips, time.Now().Add(time.Duration(1) * time.Second), err
 }
 
 // QueryIP implements Server.
-func (s *LocalNameServer) QueryIP(_ context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, error) {
-	ips, _, err := s.QueryIPWithTTL(context.TODO(), domain, nil, option, false)
+func (s *LocalNameServer) QueryIP(ctx context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) ([]net.IP, error) {
+	ips, _, err := s.QueryIPWithTTL(ctx, domain, nil, option, false)
 	return ips, err
+}
+
+// NewReqID implements ServerRaw.
+func (s *LocalNameServer) NewReqID() uint16 {
+	return uint16(rand.Intn(65536))
+}
+
+// QueryRaw implements ServerRaw.
+func (s *LocalNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	return s.client.QueryRaw(request)
 }
 
 // Name implements Server.

app/dns/nameserver_quic.go
@@ -3,9 +3,9 @@
 package dns
 
 import (
-	"bytes"
 	"context"
 	"encoding/binary"
+	"io"
 	"net/url"
 	"sync"
 	"time"
@@ -182,14 +182,14 @@ func (s *QUICNameServer) updateIP(req *dnsRequest, ipRec *IPRecord) {
 	common.Must(s.cleanup.Start())
 }
 
-func (s *QUICNameServer) newReqID() uint16 {
+func (s *QUICNameServer) NewReqID() uint16 {
 	return 0
 }
 
 func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP net.IP, option dns_feature.IPOption) {
 	newError(s.name, " querying: ", domain).AtInfo().WriteToLog(session.ExportIDToError(ctx))
 
-	reqs := buildReqMsgs(domain, option, s.newReqID, genEDNS0Options(clientIP))
+	reqs := buildReqMsgs(domain, option, s.NewReqID, genEDNS0Options(clientIP))
 
 	var deadline time.Time
 	if d, ok := ctx.Deadline(); ok {
@@ -224,7 +224,7 @@ func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP 
 				return
 			}
 
-			dnsReqBuf := buf.New()
+			dnsReqBuf := buf.NewWithSize(2 + b.Len())
 			defer dnsReqBuf.Release()
 			binary.Write(dnsReqBuf, binary.BigEndian, uint16(b.Len()))
 			dnsReqBuf.Write(b.Bytes())
@@ -244,22 +244,16 @@ func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP 
 
 			_ = conn.Close()
 
-			respBuf := buf.New()
-			defer respBuf.Release()
-			n, err := respBuf.ReadFullFrom(conn, 2)
-			if err != nil && n == 0 {
-				newError("failed to read response length").Base(err).AtError().WriteToLog()
-				return
-			}
-			var length int16
-			err = binary.Read(bytes.NewReader(respBuf.Bytes()), binary.BigEndian, &length)
+			var length uint16
+			err = binary.Read(conn, binary.BigEndian, &length)
 			if err != nil {
 				newError("failed to parse response length").Base(err).AtError().WriteToLog()
 				return
 			}
-			respBuf.Clear()
-			n, err = respBuf.ReadFullFrom(conn, int32(length))
-			if err != nil && n == 0 {
+			respBuf := buf.NewWithSize(int32(length))
+			defer respBuf.Release()
+			_, err = respBuf.ReadFullFrom(conn, int32(length))
+			if err != nil {
 				newError("failed to read response length").Base(err).AtError().WriteToLog()
 				return
 			}
@@ -272,6 +266,63 @@ func (s *QUICNameServer) sendQuery(ctx context.Context, domain string, clientIP 
 			s.updateIP(r, rec)
 		}(req)
 	}
+}
+
+func (s *QUICNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	var deadline time.Time
+	if d, ok := ctx.Deadline(); ok {
+		deadline = d
+	} else {
+		deadline = time.Now().Add(time.Second * 5)
+	}
+
+	// generate new context for each req, using same context
+	// may cause reqs all aborted if any one encounter an error
+	dnsCtx := ctx
+
+	// reserve internal dns server requested Inbound
+	if inbound := session.InboundFromContext(ctx); inbound != nil {
+		dnsCtx = session.ContextWithInbound(dnsCtx, inbound)
+	}
+
+	dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
+		Protocol:       "quic",
+		SkipDNSResolve: true,
+	})
+
+	var cancel context.CancelFunc
+	dnsCtx, cancel = context.WithDeadline(dnsCtx, deadline)
+	defer cancel()
+
+	dnsReqBuf := buf.NewWithSize(2 + int32(len(request)))
+	defer dnsReqBuf.Release()
+	binary.Write(dnsReqBuf, binary.BigEndian, uint16(len(request)))
+	dnsReqBuf.Write(request)
+
+	conn, err := s.openStream(dnsCtx)
+	if err != nil {
+		return nil, newError("failed to open quic connection").Base(err)
+	}
+
+	_, err = conn.Write(dnsReqBuf.Bytes())
+	if err != nil {
+		return nil, newError("failed to send query").Base(err)
+	}
+
+	_ = conn.Close()
+
+	var length uint16
+	err = binary.Read(conn, binary.BigEndian, &length)
+	if err != nil {
+		return nil, newError("failed to parse response length").Base(err)
+	}
+	response := make([]byte, length)
+	_, err = io.ReadFull(conn, response)
+	if err != nil {
+		return nil, newError("failed to read response length").Base(err)
+	}
+
+	return response, nil
 }
 
 func (s *QUICNameServer) findIPsForDomain(domain string, option dns_feature.IPOption) ([]net.IP, time.Time, error) {

app/dns/nameserver_tcp.go
@@ -3,9 +3,9 @@
 package dns
 
 import (
-	"bytes"
 	"context"
 	"encoding/binary"
+	"io"
 	"net/url"
 	"sync"
 	"sync/atomic"
@@ -179,14 +179,14 @@ func (s *TCPNameServer) updateIP(req *dnsRequest, ipRec *IPRecord) {
 	common.Must(s.cleanup.Start())
 }
 
-func (s *TCPNameServer) newReqID() uint16 {
+func (s *TCPNameServer) NewReqID() uint16 {
 	return uint16(atomic.AddUint32(&s.reqID, 1))
 }
 
 func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP net.IP, option dns_feature.IPOption) {
 	newError(s.name, " querying DNS for: ", domain).AtDebug().WriteToLog(session.ExportIDToError(ctx))
 
-	reqs := buildReqMsgs(domain, option, s.newReqID, genEDNS0Options(clientIP))
+	reqs := buildReqMsgs(domain, option, s.NewReqID, genEDNS0Options(clientIP))
 
 	var deadline time.Time
 	if d, ok := ctx.Deadline(); ok {
@@ -218,7 +218,7 @@ func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP n
 				return
 			}
 
-			dnsReqBuf := buf.New()
+			dnsReqBuf := buf.NewWithSize(2 + b.Len())
 			defer dnsReqBuf.Release()
 			binary.Write(dnsReqBuf, binary.BigEndian, uint16(b.Len()))
 			dnsReqBuf.Write(b.Bytes())
@@ -237,22 +237,16 @@ func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP n
 				return
 			}
 
-			respBuf := buf.New()
-			defer respBuf.Release()
-			n, err := respBuf.ReadFullFrom(conn, 2)
-			if err != nil && n == 0 {
-				newError("failed to read response length").Base(err).AtError().WriteToLog()
-				return
-			}
-			var length int16
-			err = binary.Read(bytes.NewReader(respBuf.Bytes()), binary.BigEndian, &length)
+			var length uint16
+			err = binary.Read(conn, binary.BigEndian, &length)
 			if err != nil {
 				newError("failed to parse response length").Base(err).AtError().WriteToLog()
 				return
 			}
-			respBuf.Clear()
-			n, err = respBuf.ReadFullFrom(conn, int32(length))
-			if err != nil && n == 0 {
+			respBuf := buf.NewWithSize(int32(length))
+			defer respBuf.Release()
+			_, err = respBuf.ReadFullFrom(conn, int32(length))
+			if err != nil {
 				newError("failed to read response length").Base(err).AtError().WriteToLog()
 				return
 			}
@@ -266,6 +260,59 @@ func (s *TCPNameServer) sendQuery(ctx context.Context, domain string, clientIP n
 			s.updateIP(r, rec)
 		}(req)
 	}
+}
+
+func (s *TCPNameServer) QueryRaw(ctx context.Context, request []byte) ([]byte, error) {
+	var deadline time.Time
+	if d, ok := ctx.Deadline(); ok {
+		deadline = d
+	} else {
+		deadline = time.Now().Add(time.Second * 5)
+	}
+
+	dnsCtx := ctx
+
+	if inbound := session.InboundFromContext(ctx); inbound != nil {
+		dnsCtx = session.ContextWithInbound(dnsCtx, inbound)
+	}
+
+	dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
+		Protocol:       s.protocol,
+		SkipDNSResolve: true,
+	})
+
+	var cancel context.CancelFunc
+	dnsCtx, cancel = context.WithDeadline(dnsCtx, deadline)
+	defer cancel()
+
+	dnsReqBuf := buf.NewWithSize(2 + int32(len(request)))
+	defer dnsReqBuf.Release()
+
+	binary.Write(dnsReqBuf, binary.BigEndian, uint16(len(request)))
+	dnsReqBuf.Write(request)
+
+	conn, err := s.dial(dnsCtx)
+	if err != nil {
+		return nil, newError("failed to dial namesever").Base(err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(dnsReqBuf.Bytes())
+	if err != nil {
+		return nil, newError("failed to send query").Base(err)
+	}
+
+	var length uint16
+	err = binary.Read(conn, binary.BigEndian, &length)
+	if err != nil {
+		return nil, newError("failed to parse response length").Base(err)
+	}
+	response := make([]byte, length)
+	_, err = io.ReadFull(conn, response)
+	if err != nil {
+		return nil, newError("failed to read response length").Base(err)
+	}
+	return response, nil
 }
 
 func (s *TCPNameServer) findIPsForDomain(domain string, option dns_feature.IPOption) ([]net.IP, time.Time, error) {