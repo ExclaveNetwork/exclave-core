dyhkwong/v2ray-core

Add SSH outbound

main/distro/all/all.go
@@ -49,6 +49,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks_2022"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/socks"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/ssh"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/trojan"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/vless/inbound"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/vless/outbound"

proxy/ssh/errors.generated.go
@@ -1,0 +1,9 @@
+package ssh
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

ExclaveNetwork/exclave-core

main/distro/all/all.go
@@ -49,6 +49,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/proxy/shadowsocks"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/shadowsocks_2022"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/socks"
+	_ "github.com/v2fly/v2ray-core/v4/proxy/ssh"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/trojan"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/vless/inbound"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/vless/outbound"

proxy/ssh/errors.generated.go
@@ -1,0 +1,9 @@
+package ssh
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}