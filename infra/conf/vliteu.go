dyhkwong/v2ray-core

Add jsonv4 support for new options

infra/conf/v4/vliteu.go
@@ -1,0 +1,53 @@
+package v4
+
+import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v5/infra/conf/cfgcommon"
+	"github.com/v2fly/v2ray-core/v5/proxy/vlite/inbound"
+	"github.com/v2fly/v2ray-core/v5/proxy/vlite/outbound"
+)
+
+type VLiteUDPInboundConfig struct {
+	Password                    string `json:"password"`
+	ScramblePacket              bool   `json:"scramblePacket"`
+	EnableFEC                   bool   `json:"enableFEC"`
+	EnableStabilization         bool   `json:"enableStabilization"`
+	EnableRenegotiation         bool   `json:"enableRenegotiation"`
+	HandshakeMaskingPaddingSize uint32 `json:"handshakeMaskingPaddingSize"`
+}
+
+func (c *VLiteUDPInboundConfig) Build() (proto.Message, error) {
+	return &inbound.UDPProtocolConfig{
+		Password:                    c.Password,
+		ScramblePacket:              c.ScramblePacket,
+		EnableFec:                   c.EnableFEC,
+		EnableStabilization:         c.EnableStabilization,
+		EnableRenegotiation:         c.EnableRenegotiation,
+		HandshakeMaskingPaddingSize: c.HandshakeMaskingPaddingSize,
+	}, nil
+}
+
+type VLiteUDPOutboundConfig struct {
+	Address                     *cfgcommon.Address `json:"address"`
+	Port                        uint16             `json:"port"`
+	Password                    string             `json:"password"`
+	ScramblePacket              bool               `json:"scramblePacket"`
+	EnableFEC                   bool               `json:"enableFEC"`
+	EnableStabilization         bool               `json:"enableStabilization"`
+	EnableRenegotiation         bool               `json:"enableRenegotiation"`
+	HandshakeMaskingPaddingSize uint32             `json:"handshakeMaskingPaddingSize"`
+}
+
+func (c *VLiteUDPOutboundConfig) Build() (proto.Message, error) {
+	return &outbound.UDPProtocolConfig{
+		Address:                     c.Address.Build(),
+		Port:                        uint32(c.Port),
+		Password:                    c.Password,
+		ScramblePacket:              c.ScramblePacket,
+		EnableFec:                   c.EnableFEC,
+		EnableStabilization:         c.EnableStabilization,
+		EnableRenegotiation:         c.EnableRenegotiation,
+		HandshakeMaskingPaddingSize: c.HandshakeMaskingPaddingSize,
+	}, nil
+}

transport/internet/httpupgrade/httpupgrade.go
@@ -1,9 +1,8 @@
 package httpupgrade
 
 import (
-	"context"
-
 	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v5/common/errors/errorgen
@@ -11,7 +10,7 @@ import (
 const protocolName = "httpupgrade"
 
 func init() {
-	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-		return nil, newError("httpupgrade is a transport protocol.")
+	common.Must(internet.RegisterProtocolConfigCreator(protocolName, func() interface{} {
+		return new(Config)
 	}))
 }

transport/internet/request/assembly/assembly.go
@@ -1,9 +1,8 @@
 package assembly
 
 import (
-	"context"
-
 	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v5/common/errors/errorgen
@@ -11,7 +10,7 @@ import (
 const protocolName = "request"
 
 func init() {
-	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-		return nil, newError("request is a transport protocol.")
+	common.Must(internet.RegisterProtocolConfigCreator(protocolName, func() interface{} {
+		return new(Config)
 	}))
 }

ExclaveNetwork/exclave-core

infra/conf/vliteu.go
@@ -1,0 +1,53 @@
+package conf
+
+import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v4/infra/conf/cfgcommon"
+	"github.com/v2fly/v2ray-core/v4/proxy/vlite/inbound"
+	"github.com/v2fly/v2ray-core/v4/proxy/vlite/outbound"
+)
+
+type VLiteUDPInboundConfig struct {
+	Password                    string `json:"password"`
+	ScramblePacket              bool   `json:"scramblePacket"`
+	EnableFEC                   bool   `json:"enableFEC"`
+	EnableStabilization         bool   `json:"enableStabilization"`
+	EnableRenegotiation         bool   `json:"enableRenegotiation"`
+	HandshakeMaskingPaddingSize uint32 `json:"handshakeMaskingPaddingSize"`
+}
+
+func (c *VLiteUDPInboundConfig) Build() (proto.Message, error) {
+	return &inbound.UDPProtocolConfig{
+		Password:                    c.Password,
+		ScramblePacket:              c.ScramblePacket,
+		EnableFec:                   c.EnableFEC,
+		EnableStabilization:         c.EnableStabilization,
+		EnableRenegotiation:         c.EnableRenegotiation,
+		HandshakeMaskingPaddingSize: c.HandshakeMaskingPaddingSize,
+	}, nil
+}
+
+type VLiteUDPOutboundConfig struct {
+	Address                     *cfgcommon.Address `json:"address"`
+	Port                        uint16             `json:"port"`
+	Password                    string             `json:"password"`
+	ScramblePacket              bool               `json:"scramblePacket"`
+	EnableFEC                   bool               `json:"enableFEC"`
+	EnableStabilization         bool               `json:"enableStabilization"`
+	EnableRenegotiation         bool               `json:"enableRenegotiation"`
+	HandshakeMaskingPaddingSize uint32             `json:"handshakeMaskingPaddingSize"`
+}
+
+func (c *VLiteUDPOutboundConfig) Build() (proto.Message, error) {
+	return &outbound.UDPProtocolConfig{
+		Address:                     c.Address.Build(),
+		Port:                        uint32(c.Port),
+		Password:                    c.Password,
+		ScramblePacket:              c.ScramblePacket,
+		EnableFec:                   c.EnableFEC,
+		EnableStabilization:         c.EnableStabilization,
+		EnableRenegotiation:         c.EnableRenegotiation,
+		HandshakeMaskingPaddingSize: c.HandshakeMaskingPaddingSize,
+	}, nil
+}

transport/internet/httpupgrade/httpupgrade.go
@@ -4,9 +4,8 @@
 package httpupgrade
 
 import (
-	"context"
-
 	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
@@ -14,7 +13,7 @@ import (
 const protocolName = "httpupgrade"
 
 func init() {
-	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-		return nil, newError("httpupgrade is a transport protocol.")
+	common.Must(internet.RegisterProtocolConfigCreator(protocolName, func() interface{} {
+		return new(Config)
 	}))
 }

transport/internet/request/assembly/assembly.go
@@ -4,9 +4,8 @@
 package assembly
 
 import (
-	"context"
-
 	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
@@ -14,7 +13,7 @@ import (
 const protocolName = "request"
 
 func init() {
-	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-		return nil, newError("request is a transport protocol.")
+	common.Must(internet.RegisterProtocolConfigCreator(protocolName, func() interface{} {
+		return new(Config)
 	}))
 }