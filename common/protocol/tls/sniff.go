dyhkwong/v2ray-core

TLS sniffer: support sniffing TLS record fragmentation

common/protocol/tls/sniff.go
@@ -3,9 +3,12 @@ package tls
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"strings"
 
 	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/protocol"
 	"github.com/v2fly/v2ray-core/v5/common/rangelist"
 )
 
@@ -34,83 +37,97 @@ func IsValidTLSVersion(major, minor byte) bool {
 // https://github.com/golang/go/blob/master/src/crypto/tls/handshake_messages.go#L300
 func ReadClientHello(data []byte, h *SniffHeader, validRange *rangelist.RangeList) error {
 	if len(data) < 42 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	sessionIDLen := int(data[38])
 	if sessionIDLen > 32 || len(data) < 39+sessionIDLen {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[39+sessionIDLen:]
 	if len(data) < 2 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	// cipherSuiteLen is the number of bytes of cipher suite numbers. Since
 	// they are uint16s, the number must be even.
 	cipherSuiteLen := int(data[0])<<8 | int(data[1])
-	if cipherSuiteLen%2 == 1 || len(data) < 2+cipherSuiteLen {
+	if cipherSuiteLen%2 == 1 {
 		return errNotClientHello
+	}
+	if len(data) < 2+cipherSuiteLen {
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[2+cipherSuiteLen:]
 	if len(data) < 1 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	compressionMethodsLen := int(data[0])
 	if len(data) < 1+compressionMethodsLen {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[1+compressionMethodsLen:]
 
-	if len(data) == 0 {
-		return errNotClientHello
-	}
 	if len(data) < 2 {
-		return errNotClientHello
+		return protocol.ErrProtoNeedMoreData
 	}
 
-	// extensionsLength := int(data[0])<<8 | int(data[1])
+	extensionsLength := int(data[0])<<8 | int(data[1])
 	data = data[2:]
-	/*
-		if extensionsLength != len(data) {
-			return errNotClientHello
-		}
-	*/
+	dataLen := len(data)
 
 	offset := 44 + sessionIDLen + cipherSuiteLen + compressionMethodsLen
 	for len(data) != 0 {
 		if len(data) < 4 {
-			return errNotClientHello
+			if dataLen == extensionsLength {
+				return errNotClientHello
+			}
+			return protocol.ErrProtoNeedMoreData
 		}
 		extension := uint16(data[0])<<8 | uint16(data[1])
 		length := int(data[2])<<8 | int(data[3])
 		data = data[4:]
 		offset += 4
 		if len(data) < length {
-			return errNotClientHello
+			if dataLen == extensionsLength {
+				return errNotClientHello
+			}
+			return protocol.ErrProtoNeedMoreData
 		}
 
 		if extension == 0x00 { /* extensionServerName */
 			d := data[:length]
 			if len(d) < 2 {
-				return errNotClientHello
+				if dataLen == extensionsLength {
+					return errNotClientHello
+				}
+				return protocol.ErrProtoNeedMoreData
 			}
 			namesLen := int(d[0])<<8 | int(d[1])
 			d = d[2:]
 			if len(d) != namesLen {
-				return errNotClientHello
+				if dataLen == extensionsLength {
+					return errNotClientHello
+				}
+				return protocol.ErrProtoNeedMoreData
 			}
 			for len(d) > 0 {
 				if len(d) < 3 {
-					return errNotClientHello
+					if dataLen == extensionsLength {
+						return errNotClientHello
+					}
+					return protocol.ErrProtoNeedMoreData
 				}
 				nameType := d[0]
 				nameLen := int(d[1])<<8 | int(d[2])
 				d = d[3:]
 				if len(d) < nameLen {
-					return errNotClientHello
+					if dataLen == extensionsLength {
+						return errNotClientHello
+					}
+					return protocol.ErrProtoNeedMoreData
 				}
 				if nameType == 0 {
 					if validRange != nil && !validRange.In(offset+5, offset+5+nameLen) {
-						return errNotClientHello
+						return protocol.ErrProtoNeedMoreData
 					}
 					serverName := string(d[:nameLen])
 					// An SNI value may not include a
@@ -129,7 +146,10 @@ func ReadClientHello(data []byte, h *SniffHeader, validRange *rangelist.RangeLis
 		offset += length
 	}
 
-	return errNotTLS
+	if dataLen == extensionsLength {
+		return nil
+	}
+	return protocol.ErrProtoNeedMoreData
 }
 
 func SniffTLS(b []byte) (*SniffHeader, error) {
@@ -144,14 +164,45 @@ func SniffTLS(b []byte) (*SniffHeader, error) {
 		return nil, errNotTLS
 	}
 	headerLen := int(binary.BigEndian.Uint16(b[3:5]))
-	if 5+headerLen > len(b) {
-		return nil, common.ErrNoClue
+	if headerLen == 0 || headerLen >= 16384 {
+		// Implementations MUST NOT send zero-length fragments
+		// The length MUST NOT exceed 2^14
+		return nil, errNotTLS
 	}
 
 	h := &SniffHeader{}
-	err := ReadClientHello(b[5:5+headerLen], h, nil)
-	if err == nil {
-		return h, nil
+	data := buf.NewWithSize(32767)
+	defer data.Release()
+	for len(b) > 0 {
+		if data.Cap() < int32(len(b)) {
+			return nil, io.ErrShortBuffer
+		}
+		data.Write(b[5:min(5+headerLen, len(b))])
+		err := ReadClientHello(data.Bytes(), h, nil)
+		if err == nil {
+			return h, nil
+		}
+		if err == errNotTLS || err == errNotClientHello {
+			return nil, err
+		}
+		b = b[min(5+headerLen, len(b)):]
+
+		if len(b) < 5 {
+			return nil, protocol.ErrProtoNeedMoreData
+		}
+
+		if b[0] != 0x16 /* TLS Handshake */ {
+			return nil, errNotTLS
+		}
+		if !IsValidTLSVersion(b[1], b[2]) {
+			return nil, errNotTLS
+		}
+		headerLen := int(binary.BigEndian.Uint16(b[3:5]))
+		if headerLen == 0 || headerLen >= 16384 {
+			// Implementations MUST NOT send zero-length fragments
+			// The length MUST NOT exceed 2^14
+			return nil, errNotTLS
+		}
 	}
-	return nil, err
+	return nil, protocol.ErrProtoNeedMoreData
 }

common/protocol/tls/sniff_test.go
@@ -1,8 +1,10 @@
 package tls_test
 
 import (
+	"encoding/hex"
 	"testing"
 
+	"github.com/v2fly/v2ray-core/v5/common"
 	. "github.com/v2fly/v2ray-core/v5/common/protocol/tls"
 )
 
@@ -157,5 +159,20 @@ func TestTLSHeaders(t *testing.T) {
 				t.Error("expect domain ", test.domain, " but got ", header.Domain())
 			}
 		}
+	}
+}
+
+func TestSniffTLSRecordFragmentation(t *testing.T) {
+	pkt, err := hex.DecodeString(
+		"160301015e010007cf030387948e348724d19b1f785da031edc3e860a7c99cf77e8600c027bb34362c4e322091244f13182b84c3bd9525577c7cef87372ff5922aa40d94ffa557284f0e0c4e0020caca130113021303c02bc02fc02cc030cca9cca8c013c014009c009d002f0035010007664a4a0000000a000c000ababa11ec001d00170018002d0002010100230000002b000706eaea03040303000b0002010000120000fe0d00ba000001000132002063fe7acd9f1acccb5cf543dd024230584200aacc5cc6b649ea6c5590237fcc080090decc4b225c4ae5a808c29d694ededab68dae188a0ca15127b9716bba0cf69f6ea7802987ee0e47c361be4b699343584c343735b759c28f6bf994f06eca2716dae049d065439b7a1d11f27eda546eaeed770af91d6d9e07bb5e1530b7399f404ecfb7838d9cef03056f1b1c7d7e7c1e962648e6f1c86460003999eda42c26641c74ba79d46b372d63e081e2fbccecab08" +
+			"160301015e001b0003020002003304ef04edbaba00010011ec04c0804b9305a03c9e003c2d279a8c76c9f3e017c7e8386786b4a7920b07fb5c42e57134106324a27d157c8b90960519abb3746c1fd7a81030b4c801289dbc495d47109808673a07730268f6b71fb4c43d8acc8a8989391472d38b4d3525798e3a24869a1ee6d8c86f9635e3bc839fab33b5f3c42eaa6fc6a32584a7976d657dfc298bcc88763c61b5b599bd2d3415f634649c8552bdb581bc754e88883625b29282dab5f3961e84628c9a91001f415e002289cec224107a13c121860c63463a69bc03f056b9b89a06f29266324fb6c719eb7b3abc1b103285ba5aa3703cd03ea9198a6055bdc90c6139d29ff9293a5577a59eb43acaa2238f8a591ea95847bc9586c55400bc4b272ab38d462fe427af733a6885dc77a2d182b86c4063aa16aef196f1e773374577a1876115897cf2c4b1042784ef260086e5a197e3ab344a501f7026278048d63b0c03" +
+			"160301015eb6233195467c0066cac86248fabda4043f32c50645795f6ac38575b0352bb3151fa9a653f7220e5c719cf5c7be07aec33503ee86a3855215b5baa30e104bb9b537532acb0b80027cd7476efb18bacb3adc98b16d4b613a3a23ba38a9c82c7aa487721a1579c030b73443c72c8922828c3e5ac63d552997fff55620460642eb59dbe704c4e7bd40063587a29d72c45b9646bf1fb41b5d78646564bc992b1e4d788b3f33ada1f32e514c38f3324aa2519ab485b4f3b96ef306cd81a439075a41d4f3a23b0aa453d3c005854417e48305f2716412cfb1711e649a4795450148e615acfb0815f3560043255514bada1808724309d3aaa1e68bb28a38c925e4bf54eb77f189004d36560c1310481652b433a412d76db31731a1271b58c3809522bd176b4f31f29db67b0af84aa8ebc895062ba9ed822974574f8324595eab6c3c857327427345561151817e0e38ce37025f3105209b566b45f6c44f300a577b84" +
+			"160301015e77249a31fb588bebcc9f07c7bdbb5975780cebb2775c387cf24bb9fc407176717bd38c8d71bb821ef59ca29a880bc43732e320b5a0174c777626548ee640919c3c7c518cb92c56945160cb0e34620640650479327453543743a6b5539f4da02629666a5e0a5cb3624670ca81400581ef14bc2765aa7f462c6f700b85132c14e06cb24295dd72001d071904261786ab099a489221e7bbc81071c34945e846882d5067c1c27df9660673696fb628a57d6a9f09a196acb196af107f91c916589395757993c684553640a5ce3a45770aae977649f017210cd497553856add5789fd29180b336207c70aaa6c74b87c64397072af81f6bdbbb01725e8d48b045910fc5bac1144226e5d74a8509787103ba61b521f6d13dc703588ed8a68d682b88d3275e587f4e7c8071b162e4f331ec81110cb2979fb228b14c3db0451fbc288feba7bb41374c83554608e53b4bb8b508a58814e416f8b57e1e71c345cc851e36" +
+			"160301015eb444a7828c0b7a545a9b58a62cf97697973748ea4406ea5b7ff7260552162099da2dd7735642d04e7bf8bd0b87a8f8bb72acfb1a695cbb83ab513a068f6e7408cf5726b95635cf14520bb97993b23da2f8374495acda316ac1f5799af2bcebbc5592c69b48f08ce8a97ed32287d5763284131a83ba8a780cbf95c6c1c2f50732d15eb6d8ca1319bf4ad58c3452b4f6686df8a52f56dde82777bd02c93737b9afa10266d0d81c5abf4835b0f8f209f5be22f0bf9c1579f2a6677dc95e001d002091d1d7c7eb4cba1465f64bf4e380823ff2a163caedfbdd11a74d2b45cdf1bc6000000010000e00000b6578616d706c652e636f6d00170000ff010001000010000e000c02683208687474702f312e31000d001200100403080404010503080505010806060100050005010000000044cd000500030268327a7a0001000029011b00e600e000020dff9ee9d627573a04683b40000366f31bcf8c539beb2455" +
+			"16030100fd6b484cc4a9adf49be379d93c9fdc3063d085fc080b897b9208c4c2d22a013e68678a3243640212f09623b54be186e44dd3a2556a5ea0ba2eb6025e10a5dd86e6217df60546edd26068eeb1c94585f7e2306e9766cac702bad7da8f5108ef4cc0d1c6756e754b74a9f1cebb9d5f40b03a5f79fe81b566dcf65b387e6a88720e46f2cb5d03a81acc4bcbbd3b4784201a22aaca2ef8210b601c4cbee25b13183eebbbff0cc9db12284396cc7aee27997d5f595794a94170652153b68b3aa5c4b42955d8434a08234ba0bd55003130c8e525eb2f72d126d0bba2df02337c4b452a2667f008ab777728b789458d6b7e0b70447a961b7c237f62ecf26495a979")
+	common.Must(err)
+	hdr, err := SniffTLS(pkt)
+	if err != nil || hdr.Domain() != "example.com" {
+		t.Error("failed")
 	}
 }

ExclaveNetwork/exclave-core

common/protocol/tls/sniff.go
@@ -3,9 +3,12 @@ package tls
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"strings"
 
 	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/protocol"
 	"github.com/v2fly/v2ray-core/v4/common/rangelist"
 )
 
@@ -34,83 +37,97 @@ func IsValidTLSVersion(major, minor byte) bool {
 // https://github.com/golang/go/blob/master/src/crypto/tls/handshake_messages.go#L300
 func ReadClientHello(data []byte, h *SniffHeader, validRange *rangelist.RangeList) error {
 	if len(data) < 42 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	sessionIDLen := int(data[38])
 	if sessionIDLen > 32 || len(data) < 39+sessionIDLen {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[39+sessionIDLen:]
 	if len(data) < 2 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	// cipherSuiteLen is the number of bytes of cipher suite numbers. Since
 	// they are uint16s, the number must be even.
 	cipherSuiteLen := int(data[0])<<8 | int(data[1])
-	if cipherSuiteLen%2 == 1 || len(data) < 2+cipherSuiteLen {
+	if cipherSuiteLen%2 == 1 {
 		return errNotClientHello
+	}
+	if len(data) < 2+cipherSuiteLen {
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[2+cipherSuiteLen:]
 	if len(data) < 1 {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	compressionMethodsLen := int(data[0])
 	if len(data) < 1+compressionMethodsLen {
-		return common.ErrNoClue
+		return protocol.ErrProtoNeedMoreData
 	}
 	data = data[1+compressionMethodsLen:]
 
-	if len(data) == 0 {
-		return errNotClientHello
-	}
 	if len(data) < 2 {
-		return errNotClientHello
+		return protocol.ErrProtoNeedMoreData
 	}
 
-	// extensionsLength := int(data[0])<<8 | int(data[1])
+	extensionsLength := int(data[0])<<8 | int(data[1])
 	data = data[2:]
-	/*
-		if extensionsLength != len(data) {
-			return errNotClientHello
-		}
-	*/
+	dataLen := len(data)
 
 	offset := 44 + sessionIDLen + cipherSuiteLen + compressionMethodsLen
 	for len(data) != 0 {
 		if len(data) < 4 {
-			return errNotClientHello
+			if dataLen == extensionsLength {
+				return errNotClientHello
+			}
+			return protocol.ErrProtoNeedMoreData
 		}
 		extension := uint16(data[0])<<8 | uint16(data[1])
 		length := int(data[2])<<8 | int(data[3])
 		data = data[4:]
 		offset += 4
 		if len(data) < length {
-			return errNotClientHello
+			if dataLen == extensionsLength {
+				return errNotClientHello
+			}
+			return protocol.ErrProtoNeedMoreData
 		}
 
 		if extension == 0x00 { /* extensionServerName */
 			d := data[:length]
 			if len(d) < 2 {
-				return errNotClientHello
+				if dataLen == extensionsLength {
+					return errNotClientHello
+				}
+				return protocol.ErrProtoNeedMoreData
 			}
 			namesLen := int(d[0])<<8 | int(d[1])
 			d = d[2:]
 			if len(d) != namesLen {
-				return errNotClientHello
+				if dataLen == extensionsLength {
+					return errNotClientHello
+				}
+				return protocol.ErrProtoNeedMoreData
 			}
 			for len(d) > 0 {
 				if len(d) < 3 {
-					return errNotClientHello
+					if dataLen == extensionsLength {
+						return errNotClientHello
+					}
+					return protocol.ErrProtoNeedMoreData
 				}
 				nameType := d[0]
 				nameLen := int(d[1])<<8 | int(d[2])
 				d = d[3:]
 				if len(d) < nameLen {
-					return errNotClientHello
+					if dataLen == extensionsLength {
+						return errNotClientHello
+					}
+					return protocol.ErrProtoNeedMoreData
 				}
 				if nameType == 0 {
 					if validRange != nil && !validRange.In(offset+5, offset+5+nameLen) {
-						return errNotClientHello
+						return protocol.ErrProtoNeedMoreData
 					}
 					serverName := string(d[:nameLen])
 					// An SNI value may not include a
@@ -129,7 +146,10 @@ func ReadClientHello(data []byte, h *SniffHeader, validRange *rangelist.RangeLis
 		offset += length
 	}
 
-	return errNotTLS
+	if dataLen == extensionsLength {
+		return nil
+	}
+	return protocol.ErrProtoNeedMoreData
 }
 
 func SniffTLS(b []byte) (*SniffHeader, error) {
@@ -144,14 +164,45 @@ func SniffTLS(b []byte) (*SniffHeader, error) {
 		return nil, errNotTLS
 	}
 	headerLen := int(binary.BigEndian.Uint16(b[3:5]))
-	if 5+headerLen > len(b) {
-		return nil, common.ErrNoClue
+	if headerLen == 0 || headerLen >= 16384 {
+		// Implementations MUST NOT send zero-length fragments
+		// The length MUST NOT exceed 2^14
+		return nil, errNotTLS
 	}
 
 	h := &SniffHeader{}
-	err := ReadClientHello(b[5:5+headerLen], h, nil)
-	if err == nil {
-		return h, nil
+	data := buf.NewWithSize(32767)
+	defer data.Release()
+	for len(b) > 0 {
+		if data.Cap() < int32(len(b)) {
+			return nil, io.ErrShortBuffer
+		}
+		data.Write(b[5:min(5+headerLen, len(b))])
+		err := ReadClientHello(data.Bytes(), h, nil)
+		if err == nil {
+			return h, nil
+		}
+		if err == errNotTLS || err == errNotClientHello {
+			return nil, err
+		}
+		b = b[min(5+headerLen, len(b)):]
+
+		if len(b) < 5 {
+			return nil, protocol.ErrProtoNeedMoreData
+		}
+
+		if b[0] != 0x16 /* TLS Handshake */ {
+			return nil, errNotTLS
+		}
+		if !IsValidTLSVersion(b[1], b[2]) {
+			return nil, errNotTLS
+		}
+		headerLen := int(binary.BigEndian.Uint16(b[3:5]))
+		if headerLen == 0 || headerLen >= 16384 {
+			// Implementations MUST NOT send zero-length fragments
+			// The length MUST NOT exceed 2^14
+			return nil, errNotTLS
+		}
 	}
-	return nil, err
+	return nil, protocol.ErrProtoNeedMoreData
 }

common/protocol/tls/sniff_test.go
@@ -1,8 +1,10 @@
 package tls_test
 
 import (
+	"encoding/hex"
 	"testing"
 
+	"github.com/v2fly/v2ray-core/v4/common"
 	. "github.com/v2fly/v2ray-core/v4/common/protocol/tls"
 )
 
@@ -157,5 +159,20 @@ func TestTLSHeaders(t *testing.T) {
 				t.Error("expect domain ", test.domain, " but got ", header.Domain())
 			}
 		}
+	}
+}
+
+func TestSniffTLSRecordFragmentation(t *testing.T) {
+	pkt, err := hex.DecodeString(
+		"160301015e010007cf030387948e348724d19b1f785da031edc3e860a7c99cf77e8600c027bb34362c4e322091244f13182b84c3bd9525577c7cef87372ff5922aa40d94ffa557284f0e0c4e0020caca130113021303c02bc02fc02cc030cca9cca8c013c014009c009d002f0035010007664a4a0000000a000c000ababa11ec001d00170018002d0002010100230000002b000706eaea03040303000b0002010000120000fe0d00ba000001000132002063fe7acd9f1acccb5cf543dd024230584200aacc5cc6b649ea6c5590237fcc080090decc4b225c4ae5a808c29d694ededab68dae188a0ca15127b9716bba0cf69f6ea7802987ee0e47c361be4b699343584c343735b759c28f6bf994f06eca2716dae049d065439b7a1d11f27eda546eaeed770af91d6d9e07bb5e1530b7399f404ecfb7838d9cef03056f1b1c7d7e7c1e962648e6f1c86460003999eda42c26641c74ba79d46b372d63e081e2fbccecab08" +
+			"160301015e001b0003020002003304ef04edbaba00010011ec04c0804b9305a03c9e003c2d279a8c76c9f3e017c7e8386786b4a7920b07fb5c42e57134106324a27d157c8b90960519abb3746c1fd7a81030b4c801289dbc495d47109808673a07730268f6b71fb4c43d8acc8a8989391472d38b4d3525798e3a24869a1ee6d8c86f9635e3bc839fab33b5f3c42eaa6fc6a32584a7976d657dfc298bcc88763c61b5b599bd2d3415f634649c8552bdb581bc754e88883625b29282dab5f3961e84628c9a91001f415e002289cec224107a13c121860c63463a69bc03f056b9b89a06f29266324fb6c719eb7b3abc1b103285ba5aa3703cd03ea9198a6055bdc90c6139d29ff9293a5577a59eb43acaa2238f8a591ea95847bc9586c55400bc4b272ab38d462fe427af733a6885dc77a2d182b86c4063aa16aef196f1e773374577a1876115897cf2c4b1042784ef260086e5a197e3ab344a501f7026278048d63b0c03" +
+			"160301015eb6233195467c0066cac86248fabda4043f32c50645795f6ac38575b0352bb3151fa9a653f7220e5c719cf5c7be07aec33503ee86a3855215b5baa30e104bb9b537532acb0b80027cd7476efb18bacb3adc98b16d4b613a3a23ba38a9c82c7aa487721a1579c030b73443c72c8922828c3e5ac63d552997fff55620460642eb59dbe704c4e7bd40063587a29d72c45b9646bf1fb41b5d78646564bc992b1e4d788b3f33ada1f32e514c38f3324aa2519ab485b4f3b96ef306cd81a439075a41d4f3a23b0aa453d3c005854417e48305f2716412cfb1711e649a4795450148e615acfb0815f3560043255514bada1808724309d3aaa1e68bb28a38c925e4bf54eb77f189004d36560c1310481652b433a412d76db31731a1271b58c3809522bd176b4f31f29db67b0af84aa8ebc895062ba9ed822974574f8324595eab6c3c857327427345561151817e0e38ce37025f3105209b566b45f6c44f300a577b84" +
+			"160301015e77249a31fb588bebcc9f07c7bdbb5975780cebb2775c387cf24bb9fc407176717bd38c8d71bb821ef59ca29a880bc43732e320b5a0174c777626548ee640919c3c7c518cb92c56945160cb0e34620640650479327453543743a6b5539f4da02629666a5e0a5cb3624670ca81400581ef14bc2765aa7f462c6f700b85132c14e06cb24295dd72001d071904261786ab099a489221e7bbc81071c34945e846882d5067c1c27df9660673696fb628a57d6a9f09a196acb196af107f91c916589395757993c684553640a5ce3a45770aae977649f017210cd497553856add5789fd29180b336207c70aaa6c74b87c64397072af81f6bdbbb01725e8d48b045910fc5bac1144226e5d74a8509787103ba61b521f6d13dc703588ed8a68d682b88d3275e587f4e7c8071b162e4f331ec81110cb2979fb228b14c3db0451fbc288feba7bb41374c83554608e53b4bb8b508a58814e416f8b57e1e71c345cc851e36" +
+			"160301015eb444a7828c0b7a545a9b58a62cf97697973748ea4406ea5b7ff7260552162099da2dd7735642d04e7bf8bd0b87a8f8bb72acfb1a695cbb83ab513a068f6e7408cf5726b95635cf14520bb97993b23da2f8374495acda316ac1f5799af2bcebbc5592c69b48f08ce8a97ed32287d5763284131a83ba8a780cbf95c6c1c2f50732d15eb6d8ca1319bf4ad58c3452b4f6686df8a52f56dde82777bd02c93737b9afa10266d0d81c5abf4835b0f8f209f5be22f0bf9c1579f2a6677dc95e001d002091d1d7c7eb4cba1465f64bf4e380823ff2a163caedfbdd11a74d2b45cdf1bc6000000010000e00000b6578616d706c652e636f6d00170000ff010001000010000e000c02683208687474702f312e31000d001200100403080404010503080505010806060100050005010000000044cd000500030268327a7a0001000029011b00e600e000020dff9ee9d627573a04683b40000366f31bcf8c539beb2455" +
+			"16030100fd6b484cc4a9adf49be379d93c9fdc3063d085fc080b897b9208c4c2d22a013e68678a3243640212f09623b54be186e44dd3a2556a5ea0ba2eb6025e10a5dd86e6217df60546edd26068eeb1c94585f7e2306e9766cac702bad7da8f5108ef4cc0d1c6756e754b74a9f1cebb9d5f40b03a5f79fe81b566dcf65b387e6a88720e46f2cb5d03a81acc4bcbbd3b4784201a22aaca2ef8210b601c4cbee25b13183eebbbff0cc9db12284396cc7aee27997d5f595794a94170652153b68b3aa5c4b42955d8434a08234ba0bd55003130c8e525eb2f72d126d0bba2df02337c4b452a2667f008ab777728b789458d6b7e0b70447a961b7c237f62ecf26495a979")
+	common.Must(err)
+	hdr, err := SniffTLS(pkt)
+	if err != nil || hdr.Domain() != "example.com" {
+		t.Error("failed")
 	}
 }