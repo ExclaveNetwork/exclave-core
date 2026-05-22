dyhkwong/v2ray-core

Do not build with TUN by default

infra/conf/v4/tun_stub.go
@@ -1,0 +1,13 @@
+//go:build !tun || !linux || !(amd64 || arm64)
+
+package v4
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+type TUNConfig struct{}
+
+func (t *TUNConfig) Build() (proto.Message, error) { // nolint:staticcheck
+	return nil, newError("TUN unsupported")
+}

main/distro/all/tun.go
@@ -1,0 +1,7 @@
+//go:build tun && linux && (amd64 || arm64)
+
+package all
+
+import (
+	_ "github.com/v2fly/v2ray-core/v5/app/tun"
+)

ExclaveNetwork/exclave-core

infra/conf/tun_stub.go
@@ -1,0 +1,13 @@
+//go:build !tun || !linux || !(amd64 || arm64)
+
+package conf
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+type TUNConfig struct{}
+
+func (t *TUNConfig) Build() (proto.Message, error) { // nolint:staticcheck
+	return nil, newError("TUN unsupported")
+}

main/distro/all/tun.go
@@ -1,0 +1,7 @@
+//go:build tun && linux && (amd64 || arm64)
+
+package all
+
+import (
+	_ "github.com/v2fly/v2ray-core/v4/app/tun"
+)