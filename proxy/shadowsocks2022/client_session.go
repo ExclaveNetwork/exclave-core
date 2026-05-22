dyhkwong/v2ray-core

Add buffer.Endpoint support for MonoDestUDPConn

proxy/shadowsocks2022/client_session.go
@@ -13,6 +13,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/udp"
 )
 
 func NewClientUDPSession(ctx context.Context, conn io.ReadWriteCloser, packetProcessor UDPClientPacketProcessor) *ClientUDPSession {
@@ -230,12 +231,28 @@ func (c *ClientUDPSessionConn) Close() error {
 func (c *ClientUDPSessionConn) WriteTo(p []byte, addr gonet.Addr) (n int, err error) {
 	thisPacketID := c.nextWritePacketID
 	c.nextWritePacketID += 1
+	var address net.Address
+	var port int
+	if udpAddr, ok := addr.(*net.UDPAddr); ok {
+		address = net.IPAddress(udpAddr.IP)
+		port = udpAddr.Port
+	} else if monoDestUDPAddr, ok := addr.(*udp.MonoDestUDPAddr); ok {
+		address = monoDestUDPAddr.Address
+		port = int(monoDestUDPAddr.Port)
+	} else {
+		dest, err := net.ParseDestination(addr.Network() + ":" + addr.String())
+		if err != nil {
+			return 0, newError("unable to parse destination").Base(err)
+		}
+		address = dest.Address
+		port = int(dest.Port)
+	}
 	req := &UDPRequest{
 		SessionID: [8]byte{},
 		PacketID:  thisPacketID,
 		TimeStamp: uint64(time.Now().Unix()),
-		Address:   net.IPAddress(addr.(*gonet.UDPAddr).IP),
-		Port:      addr.(*net.UDPAddr).Port,
+		Address:   address,
+		Port:      port,
 		Payload:   nil,
 	}
 	copy(req.SessionID[:], c.sessionID)
@@ -285,7 +302,12 @@ func (c *ClientUDPSessionConn) ReadFrom(p []byte) (n int, addr net.Addr, err err
 			}
 			trackedState.lastSeen = time.Now()
 
-			addr = &net.UDPAddr{IP: resp.Address.IP(), Port: resp.Port}
+			if !resp.Address.Family().IsDomain() {
+				addr = &net.UDPAddr{IP: resp.Address.IP(), Port: resp.Port}
+			} else {
+				port, _ := net.PortFromInt(uint32(resp.Port))
+				addr = &udp.MonoDestUDPAddr{Address: resp.Address, Port: port}
+			}
 		}
 		return n, addr, nil
 	}

transport/internet/udp/monodest.go
@@ -6,6 +6,19 @@ import (
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
 
+type MonoDestUDPAddr struct {
+	Address net.Address
+	Port    net.Port
+}
+
+func (*MonoDestUDPAddr) Network() string {
+	return "udp"
+}
+
+func (a *MonoDestUDPAddr) String() string {
+	return a.Address.String() + ":" + a.Port.String()
+}
+
 func NewMonoDestUDPConn(conn internet.AbstractPacketConn, addr net.Addr) *MonoDestUDPConn {
 	return &MonoDestUDPConn{
 		AbstractPacketConn: conn,
@@ -21,18 +34,50 @@ type MonoDestUDPConn struct {
 func (m *MonoDestUDPConn) ReadMultiBuffer() (buf.MultiBuffer, error) {
 	buffer := buf.New()
 	buffer.Extend(buf.Size)
-	nBytes, _, err := m.AbstractPacketConn.ReadFrom(buffer.Bytes())
+	nBytes, addr, err := m.AbstractPacketConn.ReadFrom(buffer.Bytes())
 	if err != nil {
 		buffer.Release()
 		return nil, err
 	}
 	buffer.Resize(0, int32(nBytes))
+	if udpAddr, ok := addr.(*net.UDPAddr); ok {
+		buffer.Endpoint = &net.Destination{
+			Address: net.IPAddress(udpAddr.IP),
+			Port:    net.Port(udpAddr.Port),
+			Network: net.Network_UDP,
+		}
+	} else if monoDestUDPAddr, ok := addr.(*MonoDestUDPAddr); ok {
+		buffer.Endpoint = &net.Destination{
+			Address: monoDestUDPAddr.Address,
+			Port:    monoDestUDPAddr.Port,
+			Network: net.Network_UDP,
+		}
+	} else {
+		dest, err := net.ParseDestination(addr.Network() + ":" + addr.String())
+		if err != nil {
+			buffer.Release()
+			return nil, newError("unable to parse destination").Base(err)
+		}
+		buffer.Endpoint = &net.Destination{
+			Address: dest.Address,
+			Port:    dest.Port,
+			Network: net.Network_UDP,
+		}
+	}
 	return buf.MultiBuffer{buffer}, nil
 }
 
 func (m *MonoDestUDPConn) WriteMultiBuffer(buffer buf.MultiBuffer) error {
 	for _, b := range buffer {
-		_, err := m.AbstractPacketConn.WriteTo(b.Bytes(), m.dest)
+		dest := m.dest
+		if b.Endpoint != nil {
+			if !b.Endpoint.Address.Family().IsDomain() {
+				dest = &net.UDPAddr{IP: b.Endpoint.Address.IP(), Port: int(b.Endpoint.Port)}
+			} else {
+				dest = &MonoDestUDPAddr{Address: b.Endpoint.Address, Port: b.Endpoint.Port}
+			}
+		}
+		_, err := m.AbstractPacketConn.WriteTo(b.Bytes(), dest)
 		if err != nil {
 			return err
 		}

ExclaveNetwork/exclave-core

proxy/shadowsocks2022/client_session.go
@@ -15,6 +15,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common/buf"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/udp"
 )
 
 func NewClientUDPSession(ctx context.Context, conn io.ReadWriteCloser, packetProcessor UDPClientPacketProcessor) *ClientUDPSession {
@@ -232,12 +233,29 @@ func (c *ClientUDPSessionConn) Close() error {
 func (c *ClientUDPSessionConn) WriteTo(p []byte, addr gonet.Addr) (n int, err error) {
 	thisPacketID := c.nextWritePacketID
 	c.nextWritePacketID += 1
+	var address net.Address
+	var port int
+	switch addr := addr.(type) {
+	case *net.UDPAddr:
+		address = net.IPAddress(addr.IP)
+		port = addr.Port
+	case *udp.MonoDestUDPAddr:
+		address = addr.Address
+		port = int(addr.Port)
+	default:
+		dest, err := net.ParseDestination(addr.String())
+		if err != nil {
+			return 0, newError("unable to parse destination").Base(err)
+		}
+		address = dest.Address
+		port = int(dest.Port)
+	}
 	req := &UDPRequest{
 		SessionID: [8]byte{},
 		PacketID:  thisPacketID,
 		TimeStamp: uint64(time.Now().Unix()),
-		Address:   net.IPAddress(addr.(*gonet.UDPAddr).IP),
-		Port:      addr.(*net.UDPAddr).Port,
+		Address:   address,
+		Port:      port,
 		Payload:   nil,
 	}
 	copy(req.SessionID[:], c.sessionID)
@@ -287,7 +305,7 @@ func (c *ClientUDPSessionConn) ReadFrom(p []byte) (n int, addr net.Addr, err err
 			}
 			trackedState.lastSeen = time.Now()
 
-			addr = &net.UDPAddr{IP: resp.Address.IP(), Port: resp.Port}
+			addr = udp.NewMonoDestUDPAddr(resp.Address, net.Port(resp.Port))
 		}
 		return n, addr, nil
 	}

transport/internet/udp/monodest.go
@@ -8,6 +8,29 @@ import (
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
 
+func NewMonoDestUDPAddr(address net.Address, port net.Port) net.Addr {
+	if !address.Family().IsDomain() {
+		return &net.UDPAddr{IP: address.IP(), Port: int(port)}
+	}
+	return &MonoDestUDPAddr{
+		Address: address,
+		Port:    port,
+	}
+}
+
+type MonoDestUDPAddr struct {
+	Address net.Address
+	Port    net.Port
+}
+
+func (*MonoDestUDPAddr) Network() string {
+	return "udp"
+}
+
+func (a *MonoDestUDPAddr) String() string {
+	return a.Address.String() + ":" + a.Port.String()
+}
+
 func NewMonoDestUDPConn(conn internet.AbstractPacketConn, addr net.Addr) *MonoDestUDPConn {
 	return &MonoDestUDPConn{
 		AbstractPacketConn: conn,
@@ -23,19 +46,48 @@ type MonoDestUDPConn struct {
 func (m *MonoDestUDPConn) ReadMultiBuffer() (buf.MultiBuffer, error) {
 	buffer := buf.New()
 	buffer.Extend(buf.Size)
-	nBytes, _, err := m.ReadFrom(buffer.Bytes())
+	nBytes, addr, err := m.ReadFrom(buffer.Bytes())
 	if err != nil {
 		buffer.Release()
 		return nil, err
 	}
 	buffer.Resize(0, int32(nBytes))
+	switch addr := addr.(type) {
+	case *net.UDPAddr:
+		buffer.Endpoint = &net.Destination{
+			Address: net.IPAddress(addr.IP),
+			Port:    net.Port(addr.Port),
+			Network: net.Network_UDP,
+		}
+	case *MonoDestUDPAddr:
+		buffer.Endpoint = &net.Destination{
+			Address: addr.Address,
+			Port:    addr.Port,
+			Network: net.Network_UDP,
+		}
+	default:
+		dest, err := net.ParseDestination(addr.Network() + ":" + addr.String())
+		if err != nil {
+			buffer.Release()
+			return nil, newError("unable to parse destination").Base(err)
+		}
+		buffer.Endpoint = &net.Destination{
+			Address: dest.Address,
+			Port:    dest.Port,
+			Network: net.Network_UDP,
+		}
+	}
 	return buf.MultiBuffer{buffer}, nil
 }
 
 func (m *MonoDestUDPConn) WriteMultiBuffer(buffer buf.MultiBuffer) error {
 	defer buf.ReleaseMulti(buffer)
 	for _, b := range buffer {
-		_, err := m.WriteTo(b.Bytes(), m.dest)
+		dest := m.dest
+		if b.Endpoint != nil {
+			dest = NewMonoDestUDPAddr(b.Endpoint.Address, b.Endpoint.Port)
+		}
+		_, err := m.WriteTo(b.Bytes(), dest)
 		if err != nil {
 			return err
 		}