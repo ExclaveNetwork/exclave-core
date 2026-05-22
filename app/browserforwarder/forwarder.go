dyhkwong/v2ray-core

Remove securedload for browserforwarder

app/browserforwarder/forwarder.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -12,7 +13,8 @@ import (
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/net"
-	"github.com/v2fly/v2ray-core/v5/common/platform/securedload"
+	"github.com/v2fly/v2ray-core/v5/common/platform"
+	"github.com/v2fly/v2ray-core/v5/common/platform/filesystem"
 	"github.com/v2fly/v2ray-core/v5/features/extension"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
@@ -108,7 +110,8 @@ func BridgeResource(rw http.ResponseWriter, r *http.Request, path string) {
 	if content == "" {
 		content = "index.html"
 	}
-	data, err := securedload.GetAssetSecured("browserforwarder/" + content)
+	filename := filepath.Join("browserforwarder", content)
+	data, err := filesystem.ReadFile(platform.GetAssetLocation(filename))
 	if err != nil {
 		err = newError("cannot load necessary resources").Base(err)
 		http.Error(rw, err.Error(), http.StatusForbidden)

ExclaveNetwork/exclave-core

app/browserforwarder/forwarder.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -14,7 +15,8 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/net"
-	"github.com/v2fly/v2ray-core/v4/common/platform/securedload"
+	"github.com/v2fly/v2ray-core/v4/common/platform"
+	"github.com/v2fly/v2ray-core/v4/common/platform/filesystem"
 	"github.com/v2fly/v2ray-core/v4/features/extension"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
@@ -110,7 +112,8 @@ func BridgeResource(rw http.ResponseWriter, r *http.Request, path string) {
 	if content == "" {
 		content = "index.html"
 	}
-	data, err := securedload.GetAssetSecured("browserforwarder/" + content)
+	filename := filepath.Join("browserforwarder", content)
+	data, err := filesystem.ReadFile(platform.GetAssetLocation(filename))
 	if err != nil {
 		err = newError("cannot load necessary resources").Base(err)
 		http.Error(rw, err.Error(), http.StatusForbidden)