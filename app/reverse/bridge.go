dyhkwong/v2ray-core

Fix buffer release

app/reverse/bridge.go
@@ -6,6 +6,7 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
+	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/mux"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/session"
@@ -166,6 +167,7 @@ func (w *BridgeWorker) handleInternalConn(link transport.Link) {
 					w.state = ctl.State
 				}
 			}
+			buf.ReleaseMulti(mb)
 		}
 	}()
 }

ExclaveNetwork/exclave-core

app/reverse/bridge.go
@@ -8,6 +8,7 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
+	"github.com/v2fly/v2ray-core/v4/common/buf"
 	"github.com/v2fly/v2ray-core/v4/common/mux"
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/session"
@@ -168,6 +169,7 @@ func (w *BridgeWorker) handleInternalConn(link transport.Link) {
 					w.state = ctl.State
 				}
 			}
+			buf.ReleaseMulti(mb)
 		}
 	}()
 }