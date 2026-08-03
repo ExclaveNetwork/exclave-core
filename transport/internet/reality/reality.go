6e4e5f1872f77acb8212a0c10ebc24e4d99c7ec5
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-08-04 02:29:43 +0800

    Register REALITY Config

--- transport/internet/reality/reality.go
+++ transport/internet/reality/reality.go
@@ -1,3 +1,15 @@
 package reality
 
+import (
+	"context"
+
+	"github.com/exclavenetwork/exclave-core/v5/common"
+)
+
 //go:generate go run github.com/exclavenetwork/exclave-core/v5/common/errors/errorgen
+
+func init() {
+	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return config, nil
+	}))
+}
