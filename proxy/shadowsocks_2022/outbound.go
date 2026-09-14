5f040d9f1ecbfdd3a2cb7a705715c48f8f476302
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-09-14 11:43:12 +0800

    Unify Shadowsocks 2022 server-speaks-first protocol timeout

--- proxy/shadowsocks_2022/outbound.go
+++ proxy/shadowsocks_2022/outbound.go
@@ -3,7 +3,6 @@ package shadowsocks_2022 // nolint:stylecheck
 import (
 	"context"
 	"strconv"
-	"time"
 
 	shadowsocks "github.com/sagernet/sing-shadowsocks2"
 	"github.com/sagernet/sing-shadowsocks2/cipher"
@@ -149,7 +148,7 @@ func (o *Outbound) Process(ctx context.Context, link *transport.Link, dialer int
 		serverConn := o.method.DialEarlyConn(connection, singbridge.ToSocksAddr(destination))
 		var handshake bool
 		if timeoutReader, isTimeoutReader := link.Reader.(buf.TimeoutReader); isTimeoutReader {
-			mb, err := timeoutReader.ReadMultiBufferTimeout(time.Millisecond * 100)
+			mb, err := timeoutReader.ReadMultiBufferTimeout(proxy.FirstPayloadTimeout)
 			if err != nil && err != buf.ErrNotTimeoutReader && err != buf.ErrReadTimeout {
 				return newError("read payload").Base(err)
 			}
